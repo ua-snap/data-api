6ff631909b68977707a75c52e46665068f324719
Bob Torgerson <rltorgerson@alaska.edu>
2021-07-30 16:30:14 -0800

    Adds CORS permissions to API.

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
 import asyncio
 from aiohttp import ClientSession
 from flask import Flask, render_template
+from flask_cors import CORS
 from routes import *
 
 # Elastic Beanstalk wants `application` to be present.
 application = app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(routes)
 
