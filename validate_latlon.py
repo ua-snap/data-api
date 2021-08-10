0efcf5abb4d5dee78a15fc3c4b36ba42145832aa
kyleredilla <kmredilla@alaska.edu>
2021-08-10 12:39:50 -0800

    add function for reprojecting lat,lon

--- validate_latlon.py
+++ validate_latlon.py
@@ -1,10 +1,15 @@
-# A module to validate latitude and longitude
-# This function will get used across a variety of endpoints.
+"""
+A module to validate latitude and longitude, contains
+other functions that could be used across multiple endpoints.
+"""
+
+from pyproj import Transformer
 
 
 def validate(lat, lon):
     """Validate the lat and lon values,
-    return bool for validity"""
+    return bool for validity
+    """
     try:
         lat_in_ak_bbox = 51.229 <= float(lat) <= 71.3526
         lon_in_ak_bbox = -179.1506 <= float(lon) <= -129.9795
@@ -12,3 +17,20 @@ def validate(lat, lon):
     except ValueError:
         valid = False
     return valid
+
+
+def reproject_latlon(lat, lon, dst_crs):
+    """Reproject lat lon coords
+
+    Args:
+        lat (float): latitude
+        lon (float): longitude
+        dst_crs (int): EPSG code for the destination
+            coordinate reference system
+
+    Returns:
+        Reprojected coordinates in order x, y
+    """
+    transformer = Transformer.from_crs(4326, dst_crs)
+
+    return transformer.transform(lat, lon)
