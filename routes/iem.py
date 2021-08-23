7bf8a17e09d93f6d3d3019cbf535630a4922816f
Bruce Crevensten <bruce.crevensten@gmail.com>
2021-08-23 13:26:23 -0700

    IEM endpoint edits (#19)
    * Add IEM endpoint to catalog (root API endpoint)
    
    * Add rounding of IEM data values, + lint.
    
    * Adds very basic logging + timing for one endpoint.
    
    * Add comments to address tomorrow
    
    * Modification to the code creating the HUC including modifying titles to variables tas and pr.
    
    * Python Black formatting changes.
    
    * Comment changes for function aggregate_dataarray.
    
    Co-authored-by: Bob Torgerson <rltorgerson@alaska.edu>

--- routes/iem.py
+++ routes/iem.py
@@ -1,11 +1,12 @@
 import asyncio
 import io
+import time
 import itertools
 import numpy as np
 import geopandas as gpd
 import xarray as xr
 from aiohttp import ClientSession
-from flask import abort, Blueprint, render_template
+from flask import abort, Blueprint, render_template, current_app as app
 from rasterstats import zonal_stats
 from validate_latlon import validate, validate_bbox, project_latlon
 from . import routes
@@ -27,7 +28,7 @@ dim_encodings = {
 
 
 async def make_request(url, session):
-    """Make an awaitable GET request to URL, 
+    """Make an awaitable GET request to URL,
     return result based on encoding
 
     Args:
@@ -62,7 +63,7 @@ async def fetch_point_data(x, y):
         xarray.DataSet containing results of WCS netCDF query
     """
     url = f"{RAS_BASE_URL}/ows?&SERVICE=WCS&VERSION=2.0.1&REQUEST=GetCoverage&COVERAGEID=iem_temp_precip_wms&SUBSET=X({x})&SUBSET=Y({y})&FORMAT=application/json"
-
+    print(url)
     async with ClientSession() as session:
         point_data = await asyncio.create_task(make_request(url, session))
 
@@ -81,22 +82,31 @@ async def fetch_bbox_netcdf(x1, y1, x2, y2):
     """
     # only see this ever being a single request
     url = f"{RAS_BASE_URL}/ows?&SERVICE=WCS&VERSION=2.0.1&REQUEST=GetCoverage&COVERAGEID=iem_temp_precip_wms&SUBSET=X({x1},{x2}))&SUBSET=Y({y1},{y2})&FORMAT=application/netcdf"
-
+    print(url)
+    start_time = time.time()
     async with ClientSession() as session:
         netcdf_bytes = await asyncio.create_task(make_request(url, session))
 
+    app.logger.info(
+        f"Fetched BBOX data from Rasdaman, elapsed time {(time.time() - start_time)}s"
+    )
+
     # create xarray.DataSet from bytestring
     ds = xr.open_dataset(io.BytesIO(netcdf_bytes))
+
+    # TODO: This is a temporary workaround to retitle the bands coming from
+    # Rasdaman. It appears to be a bug with the returned NetCDF output.
+    ds = ds.rename_vars({"tas": "pr", "pr": "tas"})
 
     return ds
 
 
 def package_point_data(point_data):
     """Add dim names to JSON response from point query
-    
+
     Args:
         point_data (json)
-    
+
     Returns:
         Dict with dimension name
     """
@@ -128,59 +138,69 @@ def package_point_data(point_data):
                         # just a way to make sure this snippet is removed
                         # error out if the corresponding JSON scenario index value
                         # ever does equal 2.
-                        exit("RCP 6.5 must have been added - emove this code piece!")
+                        exit("RCP 6.5 must have been added - remove this code piece!")
                     scenario = dim_encodings["scenario"][sci]
                     point_data_pkg[period][season][model][scenario] = {}
 
                     for variable, value in zip(variables, values.split(" ")):
                         point_data_pkg[period][season][model][scenario][
                             variable
-                        ] = value
+                        ] = round(float(value), 1)
 
     return point_data_pkg
 
 
-def aggregate_dataarray(da, variables, poly, transform):
+def aggregate_dataarray(ds, dimensions, poly, transform):
     """Perform a spatial agrgegation of a data array within a polygon.
     Only supports mean aggregation for now.
 
     Args:
-        da (xarray.DataArray): datacube for individual variable
-        variables (list): string names of variables in DataArray
+        ds (xarray.DataSet): datacube for all variables
+        dimensions (list): string names of variables in DataSet
         poly (shapely.Polygon): polygon from shapefile
         transform (affine.Affine): affine transform raster subset
-    
+
     Returns:
         results of aggregation as a JSON-like dict
     """
-    dim_combos = itertools.product(*[da[variable].values for variable in variables])
+
+    dim_combos = itertools.product(*[ds[dimension].values for dimension in dimensions])
     # use nested for loop to construct results dict like json output for single point
     aggr_results = {}
     # build aggregate results dict for JSON output
     # hardcoded assuming same 4 dimensions,
     #   consider revising with more robust approach
-    for period_value in np.int32(da[variables[0]].values):
+
+    for period_value in np.int32(ds[dimensions[0]].values):
         period = dim_encodings["period"][period_value]
         aggr_results[period] = {}
-        for season_value in np.int32(da[variables[1]].values):
+        for season_value in np.int32(ds[dimensions[1]].values):
             season = dim_encodings["season"][season_value]
             aggr_results[period][season] = {}
-            for model_value in np.int32(da[variables[2]].values):
+            for model_value in np.int32(ds[dimensions[2]].values):
                 model = dim_encodings["model"][model_value]
                 aggr_results[period][season][model] = {}
-                for scenario_value in np.int32(da[variables[3]].values):
+                for scenario_value in np.int32(ds[dimensions[3]].values):
                     scenario = dim_encodings["scenario"][scenario_value]
                     # select subset and compute aggregate
-                    arr = da.sel(
-                        period=period_value,
-                        season=season_value,
-                        model=model_value,
-                        scenario=scenario_value,
-                    ).values
-                    aggr_result = zonal_stats(
-                        poly, arr, affine=transform, nodata=np.nan, stats=["mean"],
-                    )[0]
-                    aggr_results[period][season][model][scenario] = aggr_result
+                    aggr_results[period][season][model][scenario] = {}
+                    for val in ["tas", "pr"]:
+                        arr = (
+                            ds[val]
+                            .sel(
+                                period=period_value,
+                                season=season_value,
+                                model=model_value,
+                                scenario=scenario_value,
+                            )
+                            .values
+                        )
+                        aggr_result = zonal_stats(
+                            poly, arr, affine=transform, nodata=np.nan, stats=["mean"],
+                        )[0]
+                        aggr_results[period][season][model][scenario][val] = round(
+                            aggr_result["mean"], 1
+                        )
 
     return aggr_results
 
@@ -212,7 +232,7 @@ def run_fetch_point_data(lat, lon):
 
     Returns:
         JSON of data at provided latitude and longitude
-    
+
     Notes:
         example request: http://localhost:5000/iem/point/65.0628/-146.1627
     """
@@ -238,7 +258,7 @@ def run_aggregate_huc(huc_id):
         Mean summaries of rasters within HUC
 
     Notes:
-        Rasters refers to the individual isntances of the 
+        Rasters refers to the individual isntances of the
           singular dimension combinations
     """
     # could add check here to make sure HUC is in the geodataframe
@@ -247,6 +267,7 @@ def run_aggregate_huc(huc_id):
     #   geometry column for zonal_stats
     # reproject is needed for zonal_stats and for initial bbox
     #   bounds for query
+    # TODO What if the huc_id is invalid?
     poly_gdf = huc_gdf.loc[[huc_id]][["geometry"]].to_crs(3338)
 
     poly = poly_gdf.iloc[0]["geometry"]
@@ -259,8 +280,6 @@ def run_aggregate_huc(huc_id):
     aggr_results = {}
     met_ds.rio.set_spatial_dims("X", "Y")
     transform = met_ds.rio.transform()
-    variables = ["period", "season", "model", "scenario"]
-    aggr_results["tas"] = aggregate_dataarray(met_ds["tas"], variables, poly, transform)
-    aggr_results["pr"] = aggregate_dataarray(met_ds["pr"], variables, poly, transform)
-
+    dimensions = ["period", "season", "model", "scenario"]
+    aggr_results = aggregate_dataarray(met_ds, dimensions, poly, transform)
     return aggr_results
