67b48e5b7315b04b0cd7979e24a1cb3574e02aac
Charlie Parr <cparr4@alaska.edu>
2021-09-24 12:34:51 -0800

    Add mean annual precip endpoint (#30)
    * updating requirements.txt
    
    * add mean_annual_precip endpoint
    
    * add data source table

---
+++ routes/mean_annual_precip.py
@@ -0,0 +1,79 @@
+import asyncio
+from flask import abort, Blueprint, render_template
+from . import routes
+from validate_latlon import validate
+from fetch_data import (
+    fetch_layer_data,
+    generate_query_urls,
+    generate_base_wms_url,
+    generate_base_wfs_url,
+    fetch_data_api,
+    check_for_nodata,
+)
+from config import GS_BASE_URL
+
+mean_annual_precip_api = Blueprint("mean_annual_precip_api", __name__)
+
+wms_targets = [
+    "pr_decadal_mean_annual_total_mm_5modelAvg_rcp60_2050_2059",
+    "pr_decadal_mean_annual_total_mm_5modelAvg_rcp85_2050_2059",
+]
+wfs_targets = {}
+
+
+def package_decadal_mapr(decadal_mapr_resp):
+    """Package Mean Annual Precipitation (mm) Decadal Summaries"""
+    dec_mapr = []
+
+    for i, j in enumerate(wms_targets[0:2]):
+
+        model = j.split("_")[-4]
+        scenario = j.split("_")[-3]
+        dec_start = j.split("_")[-2]
+        dec_end = j.split("_")[-1]
+        title = f"Mean Annual Precipitation (mm) Decadal Summary Projection for {dec_start}-{dec_end}"
+        if decadal_mapr_resp[i]["features"] == []:
+            di = {"title": title, "Data Status": "No data at this location."}
+            dec_mapr.append(di)
+        else:
+            pr_mm = decadal_mapr_resp[i]["features"][0]["properties"]["GRAY_INDEX"]
+            di = {
+                "title": title,
+                "dec_start": dec_start,
+                "dec_end": dec_end,
+                "model": model,
+                "scenario": scenario,
+                "pr_mm": pr_mm,
+            }
+            check_for_nodata(di, "pr_mm", pr_mm, -9999)
+            dec_mapr.append(di)
+    return dec_mapr
+
+
+@routes.route("/mean_annual_precip/")
+@routes.route("/mean_annual_precip/abstract/")
+def mapr_about():
+    return render_template("mean_annual_precip/abstract.html")
+
+
+@routes.route("/mean_annual_precip/point/")
+def mapr_about_point():
+    return render_template("mean_annual_precip/point.html")
+
+
+@routes.route("/mean_annual_precip/point/<lat>/<lon>")
+def run_fetch_mapr_data(lat, lon):
+    """Run the async mean annual precipitation data requesting and return data as json
+    example request: http://localhost:5000/mean_annual_precipitation/65.0628/-146.1627"""
+    if not validate(lat, lon):
+        abort(400)
+    results = asyncio.run(
+        fetch_data_api(
+            GS_BASE_URL, "mean_annual_precip", wms_targets, wfs_targets, lat, lon
+        )
+    )
+    dec_mapr = package_decadal_mapr(results[0:2])
+    data = {
+        "dec_mapr": dec_mapr,
+    }
+    return data
