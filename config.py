a3cb99bb73aae8fe5bb525bc288e980f8b55fcf9
kyleredilla <kmredilla@alaska.edu>
2021-11-22 09:17:40 -0900

    switch default rasdaman server to Zeus

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ import os
 
 GS_BASE_URL = os.getenv("API_GS_BASE_URL") or "https://gs.mapventure.org/geoserver/"
 RAS_BASE_URL = (
-    os.getenv("API_RAS_BASE_URL") or "http://apollo.snap.uaf.edu:8080/rasdaman/"
+    os.getenv("API_RAS_BASE_URL") or "http://zeus.snap.uaf.edu:8080/rasdaman/"
 )
