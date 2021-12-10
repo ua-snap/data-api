b6ee69f0c81ea698b50a883c6801d28710b99f5c
Charles Parr <cparr4@alaska.edu>
2021-12-10 14:49:20 -0900

    remove deprecated iem endpoint

--- routes/__init__.py
+++ routes/__init__.py
@@ -5,7 +5,8 @@ routes = Blueprint("routes", __name__)
 from .fire import *
 from .permafrost import *
 from .huc import *
-from .iem import *
+
+# from .iem import *
 from .glacier import *
 from .geology import *
 from .physiography import *
