908f1c1525122b7e364bd19d5d5a3f0813b11585
kyleredilla <kmredilla@alaska.edu>
2021-12-13 13:13:47 -0900

    fix/remove misc iem artifacts

--- routes/__init__.py
+++ routes/__init__.py
@@ -5,8 +5,7 @@ routes = Blueprint("routes", __name__)
 from .fire import *
 from .permafrost import *
 from .huc import *
-
-# from .iem import *
+from .taspr import *
 from .glacier import *
 from .geology import *
 from .physiography import *
