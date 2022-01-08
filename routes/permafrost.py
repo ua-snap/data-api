034f1790cce53bf4ce3d30c3c5b3af52ec60bd5b
Charles Parr <cparr4@alaska.edu>
2022-01-07 16:03:10 -0900

    add permafrost endpoint for HUC zonal mean

--- routes/permafrost.py
+++ routes/permafrost.py
@@ -1,12 +1,4 @@
 import asyncio
-import io
-import operator
-import time
-from urllib.parse import quote
-import numpy as np
-import geopandas as gpd
-import xarray as xr
-from rasterstats import zonal_stats
 from flask import (
     abort,
     Blueprint,
@@ -17,38 +9,25 @@ from flask import (
 )
 
 # local imports
-from validate_latlon import validate, project_latlon
-from config import GS_BASE_URL
 from fetch_data import (
-    get_wcs_request_str,
-    generate_wcs_query_url,
-    check_for_nodata,
     fetch_data,
     fetch_data_api,
+    fetch_wcs_point_data,
+    fetch_bbox_netcdf,
+    summarize_within_poly,
 )
+from generate_requests import generate_wcs_getcov_str, generate_netcdf_wcs_getcov_str
+from generate_urls import generate_wcs_query_url
+from validate_latlon import validate, project_latlon
+from validate_data import check_for_nodata, get_huc_3338_bbox, nodata_message
+from config import GS_BASE_URL
+from luts import huc8_gdf, permafrost_encodings
 from . import routes
 
 permafrost_api = Blueprint("permafrost_api", __name__)
 
-# encodings to interpret the rasdaman coverage
-permafrost_coverage_id = "iem_gipl_magt_alt_4km_wms"
-varnames = ["magt", "alt"]
-scenarios = ["historical", "rcp45", "rcp85"]
-models = ["cruts31", "gfdlcm3", "gisse2r", "ipslcm5alr", "mricgcm3", "ncarccsm4"]
-eras = ["1995", "2025", "2050", "2075", "2095"]
-era_starts = ["1986", "2011", "2036", "2061", "2086"]
-era_ends = ["2005", "2040", "2065", "2090", "2100"]
-units_lu = {"magt": "°C", "alt": "m"}
-era_encoding = {"1995": 0, "2025": 1, "2050": 2, "2075": 3, "2095": 4}
-model_encoding = {
-    "cruts31": 0,
-    "gfdlcm3": 1,
-    "gisse2r": 2,
-    "ipslcm5alr": 3,
-    "mricgcm3": 4,
-    "ncarccsm4": 5,
-}
-scenario_encoding = {"historical": 0, "rcp45": 1, "rcp85": 2}
+# rasdaman targets
+permafrost_coverage_id = "iem_gipl_magt_alt_4km"
 
 # geoserver targets
 wms_targets = [
@@ -59,45 +38,29 @@ wfs_targets = {
     "obu_pf_extent": "PFEXTENT",
 }
 
-
-async def fetch_wcs_permafrost_point_data(x, y):
-    """Create the async request for data at the specified point.
-
-    Args:
-        x (float): lower x-coordinate bound
-        y (float): lower y-coordinate bound
-    """
-    urls = []
-    request_str = get_wcs_request_str(x, y, permafrost_coverage_id)
-    url = generate_wcs_query_url(request_str)
-    urls.append(url)
-    point_data = await fetch_data(urls)
-    return point_data
-
-
+# packaging functions unique to each query
 def package_obu_magt(obu_magt_resp):
-    """Package Obu MAGT data in dict"""
-    ds_title = "Obu et al. (2018) Mean Annual Ground Temperature (deg. C)"
+    """Package Obu MAGT raster data."""
+    ds_title = "Obu et al. (2018) Mean Annual Ground Temperature (°C)"
     if obu_magt_resp["features"] == []:
-        di = {"title": ds_title, "Data Status": "No data at this location."}
+        di = {"title": ds_title, "Data Status": nodata_message}
     else:
         depth = "Top of Permafrost"
         year = "2000-2016"
-        title = (
-            f"Obu et al. (2018) {year} Mean Annual {depth} Ground Temperature (deg. C)"
-        )
+        title = f"Obu et al. (2018) {year} Mean Annual {depth} Ground Temperature (°C)"
+        temp = obu_magt_resp["features"][0]["properties"]["GRAY_INDEX"]
+        temp = round(temp, 1)
 
-        temp = round(obu_magt_resp["features"][0]["properties"]["GRAY_INDEX"], 2)
         di = {"title": title, "year": year, "depth": depth, "temp": temp}
         check_for_nodata(di, "temp", temp, -9999)
     return di
 
 
 def package_jorgenson(jorgenson_resp):
-    """Package Jorgenson data"""
+    """Package Jorgenson vector data."""
     title = "Jorgenson et al. (2008) Permafrost Extent and Ground Ice Volume"
     if jorgenson_resp["features"] == []:
-        di = {"title": title, "Data Status": "No data at this location."}
+        di = {"title": title, "Data Status": nodata_message}
     else:
         ice = jorgenson_resp["features"][0]["properties"]["GROUNDICEV"]
         pfx = jorgenson_resp["features"][0]["properties"]["PERMAFROST"]
@@ -106,10 +69,10 @@ def package_jorgenson(jorgenson_resp):
 
 
 def package_obu_vector(obu_vector_resp):
-    """Package Obu Permafrost Extent Data"""
+    """Package Obu permafrost extent vector data."""
     title = "Obu et al. (2018) Permafrost Extent"
     if obu_vector_resp["features"] == []:
-        di = {"title": title, "Data Status": "No data at this location."}
+        di = {"title": title, "Data Status": nodata_message}
     else:
         pfx = obu_vector_resp["features"][0]["properties"]["PFEXTENT"]
         di = {"title": title, "pfx": pfx}
@@ -117,10 +80,17 @@ def package_obu_vector(obu_vector_resp):
 
 
 def package_gipl(gipl_resp):
-    """Package GIPL MAGT and ALT Data. The response here is a nested list object."""
+    """Package GIPL MAGT and ALT netCDF data.
+    The response is a nested list object."""
+    eras = list(permafrost_encodings["eras"].values())
+    models = list(permafrost_encodings["models"].values())
+    scenarios = list(permafrost_encodings["scenarios"].values())
+    varnames = permafrost_encodings["gipl_varnames"]
+
+    # Flatten this response (twice)
     flattened_resp = sum(sum(gipl_resp, []), [])
 
-    # Nested dict output structure
+    # Initialize dict structure
     di = {
         era: {
             m: {sc: {var: "value" for var in varnames} for sc in scenarios}
@@ -139,31 +109,59 @@ def package_gipl(gipl_resp):
                 di[era][model][scenario]["alt"] = float(flattened_resp[i].split(" ")[1])
                 di[era][model][scenario][
                     "title"
-                ] = "Melvin et al. (2017) GIPL 2.0 Mean Annual Ground Temperature and Active Layer Thickness Model Output"
+                ] = "Melvin et al. (2017) GIPL 2.0 Mean Annual Ground Temperature (°C) and Active Layer Thickness (m) Model Output"
                 i += 1
-    # For some reason I need to swap MAGT and ALT for the historical baseline
-    # May have to look back at the ingest to see why these are swapped
-    (
-        di["1995"]["cruts31"]["historical"]["magt"],
-        di["1995"]["cruts31"]["historical"]["alt"],
-    ) = (
-        di["1995"]["cruts31"]["historical"]["alt"],
-        di["1995"]["cruts31"]["historical"]["magt"],
-    )
-    # This block drops all the invalid dimensional combinations that are a result of jamming historical and projected data into the same data cube.
-    di["1995"].pop("gfdlcm3", None)
-    di["1995"].pop("gisse2r", None)
-    di["1995"].pop("ipslcm5alr", None)
-    di["1995"].pop("mricgcm3", None)
-    di["1995"].pop("ncarccsm4", None)
+    # This block drops all the invalid dimensional combinations that are a result of jamming historical and projected data into the same data cube. These are no data values (-9999) that should be culled.
+    models.remove("cruts31")
+    for model in models:
+        di["1995"].pop(model, None)
     di["1995"]["cruts31"].pop("rcp45", None)
     di["1995"]["cruts31"].pop("rcp85", None)
     for k in ["2025", "2050", "2075", "2095"]:
         di[k].pop("cruts31", None)
         for m in di[k].keys():
             di[k][m].pop("historical", None)
-
     return di
+
+
+def package_gipl_huc(gipl_huc_resp):
+    """Package a single data variable (GIPL MAGT or ALT)."""
+    di = gipl_huc_resp
+    eras = list(permafrost_encodings["eras"].values())
+    models = list(permafrost_encodings["models"].values())
+    scenarios = list(permafrost_encodings["scenarios"].values())
+    models.remove("cruts31")
+    for model in models:
+        di["1995"].pop(model, None)
+    di["1995"]["cruts31"].pop("rcp45", None)
+    di["1995"]["cruts31"].pop("rcp85", None)
+    for k in ["2025", "2050", "2075", "2095"]:
+        di[k].pop("cruts31", None)
+        for m in di[k].keys():
+            di[k][m].pop("historical", None)
+    return di
+
+
+def combine_gipl_huc_var_pkgs(magt_di, alt_di):
+
+    combined_gipl_di = {}
+    for era in magt_di.keys():
+        combined_gipl_di[era] = {}
+        for model in magt_di[era].keys():
+            combined_gipl_di[era][model] = {}
+            for scenario in magt_di[era][model].keys():
+                combined_gipl_di[era][model][scenario] = {}
+                combined_gipl_di[era][model][scenario]["magt"] = magt_di[era][model][
+                    scenario
+                ]
+                combined_gipl_di[era][model][scenario]["alt"] = alt_di[era][model][
+                    scenario
+                ]
+                combined_gipl_di[era][model][scenario][
+                    "title"
+                ] = "Melvin et al. (2017) GIPL 2.0 Mean Annual Ground Temperature (°C) and Active Layer Thickness (m) Model Output"
+                combined_gipl_di[era][model][scenario]["statistic"] = "Zonal Mean"
+    return combined_gipl_di
 
 
 @routes.route("/permafrost/")
@@ -174,23 +172,40 @@ def package_gipl(gipl_resp):
 @routes.route("/activelayer/abstract/")
 @routes.route("/magtalt/")
 @routes.route("/magtalt/abstract/")
+@routes.route("/magtalt/")
+@routes.route("/magtalt/abstract/")
+@routes.route("/alt/")
+@routes.route("/alt/abstract/")
+@routes.route("/magt/")
+@routes.route("/magt/abstract/")
 def pf_about():
     return render_template("permafrost/abstract.html")
 
 
+@routes.route("/permafrost/point/")
 @routes.route("/groundtemperature/point/")
 @routes.route("/activelayer/point/")
 @routes.route("/magtalt/point/")
-@routes.route("/permafrost/point/")
+@routes.route("/alt/point/")
+@routes.route("/magt/point/")
 def pf_about_point():
     return render_template("permafrost/point.html")
 
 
+@routes.route("/permafrost/huc/")
+@routes.route("/groundtemperature/huc/")
+@routes.route("/activelayer/huc/")
+@routes.route("/magtalt/huc/")
+@routes.route("/alt/huc/")
+@routes.route("/magt/huc/")
+def pf_about_huc():
+    return render_template("permafrost/huc.html")
+
+
 @routes.route("/permafrost/point/<lat>/<lon>")
-def run_fetch_all_permafrost(lat, lon):
+def run_point_fetch_all_permafrost(lat, lon):
     """Run the async request for permafrost data at a single point.
     Args:
-        varname (str): Abbreviation for the variable of interest, either "magt" or "alt"
         lat (float): latitude
         lon (float): longitude
 
@@ -207,7 +222,7 @@ def run_fetch_all_permafrost(lat, lon):
     )
 
     x, y = project_latlon(lat, lon, 3338)
-    rasdaman_results = asyncio.run(fetch_wcs_permafrost_point_data(x, y))
+    rasdaman_results = asyncio.run(fetch_wcs_point_data(x, y, permafrost_coverage_id))
 
     gipl = package_gipl(rasdaman_results)
     obu_magt = package_obu_magt(gs_results[0])
@@ -220,3 +235,35 @@ def run_fetch_all_permafrost(lat, lon):
         "jorg": jorg,
     }
     return data
+
+
+@routes.route("/permafrost/huc/<huc_id>")
+def run_huc_fetch_all_permafrost(huc_id):
+    """Endpoint to fetch data within a HUC for all permafrost data.
+
+    Args: huc_id (int): 8-digit HUC ID.
+
+    Returns:
+        huc_pkg (dict): JSON-like object containing aggregated permafrost data over the HUC.
+    """
+    poly = get_huc_3338_bbox(huc_id)
+    bounds = poly.bounds
+
+    request_str = generate_netcdf_wcs_getcov_str(
+        bounds, permafrost_coverage_id, var_coord=None
+    )
+    url = generate_wcs_query_url(request_str)
+    ds = asyncio.run(fetch_bbox_netcdf([url]))
+
+    # why are the labels and data mismatched?!?!?!
+    # this is only happening in the huc query - not point.
+    magt_poly_sum_di = summarize_within_poly(
+        ds, poly, permafrost_encodings, varname="alt"
+    )
+    alt_poly_sum_di = summarize_within_poly(
+        ds, poly, permafrost_encodings, varname="magt"
+    )
+    magt_huc_pkg = package_gipl_huc(magt_poly_sum_di)
+    alt_huc_pkg = package_gipl_huc(alt_poly_sum_di)
+    combined_pkg = combine_gipl_huc_var_pkgs(magt_huc_pkg, alt_huc_pkg)
+    return combined_pkg
