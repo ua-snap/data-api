04d2597b2e72090214a70973c5a73c96ac514068
Charlie Parr <cparr4@alaska.edu>
2022-01-14 17:26:51 -0900

    Add top level boundary endpoint (#65)
    * add ak_protected_areas shapefile and geodataframe
    
    * add boundary as top level route, remove huc from top level
    
    * initial commit for boundary template with huc nested within
    
    * initial commit for boundary endpoint for huc and protected area queries
    
    * minor docstring fix
    
    * force epsg:4326 for geojson return
    
    * force epsg:3338 on .shp loading
    
    * swap order of operations for boundary query/reproject
    
    Co-authored-by: kyleredilla <kmredilla@alaska.edu>

--- luts.py
+++ luts.py
@@ -76,4 +76,8 @@ with open("data/luts_pickles/akvegwetlandcomposite.pkl", "rb") as fp:
 
 # HUC-8 Polygons can be imported by various endpoints
 huc_src = "data/shapefiles/hydrologic_units\wbdhu8_a_ak.shp"
-huc8_gdf = gpd.read_file(huc_src).set_index("huc8")
+huc8_gdf = gpd.read_file(huc_src).set_index("huc8").to_crs(3338)
+
+# AK Protected Area Polygons can be imported by various endpoints
+akpa_src = "data/shapefiles/ak_protected_areas.shp"
+akpa_gdf = gpd.read_file(akpa_src).set_index("id").to_crs(3338)
