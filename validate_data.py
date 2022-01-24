e26d25665f5a30d838b0dc36b1750534ed9dcdc1
Charles Parr <cparr4@alaska.edu>
2022-01-24 09:59:01 -0900

    remove deprecated bbox functions

--- validate_data.py
+++ validate_data.py
@@ -22,29 +22,3 @@ def get_poly_3338_bbox(gdf, poly_id):
     poly_gdf = gdf.loc[[poly_id]][["geometry"]].to_crs(3338)
     poly = poly_gdf.iloc[0]["geometry"]
     return poly
-
-
-def get_huc_3338_bbox(huc_id):
-    """Get the Polygon Object corresponding to the HUC ID.
-
-    Args:
-        huc_id (int): 8-digit HUC ID
-    Returns:
-        poly (shapely.Polygon): Polygon object used to summarize data within. Inlcudes a 4-tuple (poly.bounds) of the bounding box enclosing the HUC polygon. Format is (xmin, ymin, xmax, ymax).
-    """
-    poly_gdf = huc8_gdf.loc[[huc_id]][["geometry"]].to_crs(3338)
-    poly = poly_gdf.iloc[0]["geometry"]
-    return poly
-
-
-def get_akpa_3338_bbox(akpa_id):
-    """Get the Polygon Object corresponding to the protected area ID.
-
-    Args:
-        akpa_id (str): ID of protected area, e.g. "FWS12"
-    Returns:
-        poly (shapely.Polygon): Polygon object used to summarize data within. Inlcudes a 4-tuple (poly.bounds) of the bounding box enclosing the polygon. Format is (xmin, ymin, xmax, ymax).
-    """
-    poly_gdf = akpa_gdf.loc[[akpa_id]][["geometry"]].to_crs(3338)
-    poly = poly_gdf.iloc[0]["geometry"]
-    return poly
