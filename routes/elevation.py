8dfb4715bfcfea56b37347886f01ab27c08855de
Charles Parr <cparr4@alaska.edu>
2022-02-08 07:48:47 -0900

    add protected area zonal query

--- routes/elevation.py
+++ routes/elevation.py
@@ -7,7 +7,6 @@ from flask import (
     request,
     current_app as app,
 )
-import numpy as np
 import rasterio as rio
 
 # local imports
@@ -52,6 +51,27 @@ def package_astergdem(astergdem_resp):
     return di
 
 
+def package_zonal_stats(src, poly):
+    transform = src.transform
+    min_band = src.read(1)
+    max_band = src.read(2)
+    mean_band = src.read(3)
+    zonal_mu = geotiff_zonal_stats(poly, mean_band, transform, ["mean"])
+    zonal_mu[0]["mean"] = int(zonal_mu[0]["mean"])
+    zonal_min = geotiff_zonal_stats(poly, min_band, transform, ["min"])
+    zonal_max = geotiff_zonal_stats(poly, max_band, transform, ["max"])
+
+    di = {
+        "title": "ASTER Global Digital Elevation Model Zonal Statistics",
+        "units": "meters difference from sea level",
+        "res": "1 kilometer",
+    }
+    di.update(zonal_min[0])
+    di.update(zonal_max[0])
+    di.update(zonal_mu[0])
+    return di
+
+
 @routes.route("/elevation/")
 @routes.route("/elevation/abstract/")
 def elevation_about():
@@ -64,12 +84,12 @@ def elevation_about_point():
 
 
 @routes.route("/elevation/huc/")
-def z_about_huc():
+def elevation_about_huc():
     return render_template("elevation/huc.html")
 
 
 @routes.route("/elevation/protectedarea/")
-def z_about_protectedarea():
+def elevation_about_protectedarea():
     return render_template("elevation/protectedarea.html")
 
 
@@ -128,23 +148,39 @@ def run_huc_fetch_all_elevation(huc_id):
 
     url = generate_wcs_query_url(request_str, GS_BASE_URL)
     with rio.open(asyncio.run(fetch_bbox_geotiff_from_gs([url]))) as src:
+        huc_pkg = package_zonal_stats(src, poly)
 
-        transform = src.transform
-        min_band = src.read(1)
-        max_band = src.read(2)
-        mean_band = src.read(3)
-        zonal_mu = geotiff_zonal_stats(poly, mean_band, transform, ["mean"])
-        zonal_mu[0]["mean"] = int(zonal_mu[0]["mean"])
-        zonal_min = geotiff_zonal_stats(poly, min_band, transform, ["min"])
-        zonal_max = geotiff_zonal_stats(poly, max_band, transform, ["max"])
+    return huc_pkg
 
-    di = {
-        "title": "ASTER Global Digital Elevation Model Zonal Statistics",
-        "units": "meters difference from sea level",
-        "res": "1 kilometer",
-    }
-    di.update(zonal_min[0])
-    di.update(zonal_max[0])
-    di.update(zonal_mu[0])
 
-    return di
+@routes.route("/elevation/protectedarea/<akpa_id>")
+def run_protectedarea_fetch_all_elevation(akpa_id):
+    """Endpoint to fetch elevation data within a protected area.
+
+    Args:
+        akpa_id (str): ID of protected area, e.g. "NPS7"
+
+    Returns:
+        pa_pkg (dict): JSON-like object of aggregated elevation data.
+    """
+    validation = validate_akpa(akpa_id)
+    if validation == 400:
+        return render_template("400/bad_request.html"), 400
+    try:
+        poly = get_poly_3338_bbox(akpa_gdf, akpa_id)
+    except:
+        return render_template("422/invalid_protected_area.html"), 422
+
+    bounds = poly.bounds
+    (x1, y1, x2, y2) = bounds
+    x = f"{x1},{x2}"
+    y = f"{y1},{y2}"
+    request_str = generate_wcs_getcov_str(
+        x, y, "astergdem_min_max_avg", var_coord=None, encoding="GeoTIFF"
+    )
+
+    url = generate_wcs_query_url(request_str, GS_BASE_URL)
+    with rio.open(asyncio.run(fetch_bbox_geotiff_from_gs([url]))) as src:
+        pa_pkg = package_zonal_stats(src, poly)
+
+    return pa_pkg
