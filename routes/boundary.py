8bc6b5fa200160b01e258d85301dfdef69cc227d
Bob Torgerson <rltorgerson@alaska.edu>
2022-03-18 11:35:03 -0800

    Updates templates for boundary and taspr for simplicity.

--- routes/boundary.py
+++ routes/boundary.py
@@ -19,42 +19,6 @@ def boundary_about():
     return render_template("boundary/abstract.html")
 
 
-@routes.route("/boundary/climatedivision/")
-@routes.route("/boundary/climatedivision/abstract/")
-def climatedivision_about():
-    return render_template("boundary/climatedivision.html")
-
-
-@routes.route("/boundary/corporation/")
-@routes.route("/boundary/corporation/abstract/")
-def corporation_about():
-    return render_template("boundary/corporation.html")
-
-
-@routes.route("/boundary/ethnolinguistic/")
-@routes.route("/boundary/ethnolinguistic/abstract/")
-def ethnolinguistic_about():
-    return render_template("boundary/ethnolinguistic.html")
-
-
-@routes.route("/boundary/firemanagement/")
-@routes.route("/boundary/firemanagement/abstract/")
-def firemanagement_about():
-    return render_template("boundary/firemanagement.html")
-
-
-@routes.route("/boundary/protectedarea/")
-@routes.route("/boundary/protectedarea/abstract/")
-def protectedarea_about():
-    return render_template("boundary/protectedarea.html")
-
-
-@routes.route("/boundary/huc/")
-@routes.route("/boundary/huc/abstract/")
-def huc_about():
-    return render_template("boundary/huc.html")
-
-
 @routes.route("/boundary/area/")
 @routes.route("/boundary/area/abstract/")
 def area_about():
