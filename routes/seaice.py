b4a6aced8460ad9e4887416e75e2e848e7fd5a6c
Bob Torgerson <rltorgerson@alaska.edu>
2022-08-31 11:04:45 -0800

    Removes code and references in documentation for the MMM endpoint for sea ice.

--- routes/seaice.py
+++ routes/seaice.py
@@ -89,38 +89,6 @@ def package_seaice_data(seaice_resp):
     return di
 
 
-def package_mmm_seaice_data(seaice_resp, start_year=None, end_year=None):
-    """Package the sea ice concentration data into a nested JSON-like dict of
-    min, mean, and max values.
-
-     Arguments:
-         seaice_resp -- the response(s) from the WCS GetCoverage request(s).
-         start_year -- starting year to find mmm
-         end_year -- ending year to find mmm
-
-     Returns:
-         di -- a nested dictionary of all sea ice concentration values
-    """
-    if None in seaice_resp:
-        return render_template("404/no_data.html"), 404
-
-    # initialize the output dict
-    di = dict()
-    start_index = 0
-    end_index = len(seaice_resp)
-    if start_year != None:
-        start_index = (int(start_year) - 1850) * 12
-    if end_year != None:
-        end_index = (int(end_year) - 1850) * 12
-
-    di["historical"] = dict()
-    di["historical"]["seaice_min"] = min(seaice_resp[start_index:end_index])
-    di["historical"]["seaice_max"] = max(seaice_resp[start_index:end_index])
-    di["historical"]["seaice_mean"] = round(np.mean(seaice_resp[start_index:end_index]))
-
-    return di
-
-
 @routes.route("/seaice/")
 @routes.route("/seaice/abstract/")
 def about_seaice():
@@ -130,48 +98,6 @@ def about_seaice():
 @routes.route("/seaice/point/")
 def about_seaice_point():
     return render_template("seaice/point.html")
-
-
-@routes.route("/mmm/seaice/")
-def about_mmm_seaice():
-    return render_template("mmm/seaice.html")
-
-
-@routes.route("/mmm/seaice/<lat>/<lon>")
-@routes.route("/mmm/seaice/<lat>/<lon>/<start_year>/<end_year>")
-def run_mmm_point_fetch_all_seaice(lat, lon, start_year=None, end_year=None):
-    """Run the async request for sea ice concentration data at a single point.
-    Finds minimum, maximum, and mean for date range if supplied.
-
-     Args:
-         lat (float): latitude
-         lon (float): longitude
-         start_year (int): starting year to find mmm
-         end_year(int): ending year to find mmm
-
-     Returns:
-         JSON-like dict of min, mean, and max of sea ice concentration data
-    """
-    validation = validate_seaice_latlon(lat, lon)
-    date_validation = validate_seaice_year(start_year, end_year)
-    if validation == 400 or date_validation == 400:
-        return render_template("400/bad_request.html"), 400
-    if validation == 422:
-        return (
-            render_template(
-                "422/invalid_latlon.html", west_bbox=WEST_BBOX, east_bbox=EAST_BBOX
-            ),
-            422,
-        )
-    x, y = project_latlon(lat, lon, 3572)
-    try:
-        rasdaman_response = asyncio.run(fetch_wcs_point_data(x, y, seaice_coverage_id))
-        pruned_data = nullify_and_prune(rasdaman_response, "seaice")
-        return package_mmm_seaice_data(pruned_data, start_year, end_year)
-    except Exception as exc:
-        if hasattr(exc, "status") and exc.status == 404:
-            return render_template("404/no_data.html"), 404
-        return render_template("500/server_error.html"), 500
 
 
 @routes.route("/seaice/point/<lat>/<lon>/")
