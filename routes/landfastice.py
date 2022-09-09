e65fa83ccf06067532f5c1269295118f467d2b7f
Charles Parr <cparr4@alaska.edu>
2022-09-09 15:53:58 -0800

    initial commit for landfast ice endpoint

---
+++ routes/landfastice.py
@@ -0,0 +1,131 @@
+import asyncio
+import calendar
+import numpy as np
+import pandas as pd
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+)
+
+# local imports
+from fetch_data import (
+    fetch_wcs_point_data,
+    get_dim_encodings,
+    build_csv_dicts,
+    write_csv,
+)
+from validate_request import (
+    validate_latlon,
+    project_latlon,
+    # validate_landfastice_year,  # we would want this, but or the ice seasons (oct to july) that are in the actual dataset.
+)
+from validate_data import nullify_and_prune, postprocess
+from . import routes
+from config import WEST_BBOX, EAST_BBOX
+
+landfastice_api = Blueprint("landfastice_api", __name__)
+landfastice_coverage_id = "landfast_sea_ice_extent"
+landfastice_encodings = asyncio.run(
+    get_dim_encodings(landfastice_coverage_id, scrape=("time", "gmlrgrid:coefficients"))
+)
+
+
+def generate_time_index(ice_season="all"):
+    """Generate a Pythonic time index for a single October-July ice season.
+
+    Arguments:
+        ice_season (str): Ice season string (YYYY) between 1996 and 2007 or 'all' for the entire time series.
+
+    Returns:
+        dt_range (pandas DatetimeIndex): a time index with daily frequency
+    """
+
+    if ice_season == "all":
+        timestamps = [x[1:-2] for x in landfastice_encodings["time"].split(" ")]
+        date_index = pd.DatetimeIndex(timestamps)
+    else:
+        start, end = landfastice_encodings["ice_season"][ice_season][0].split(", ")
+        dt_start = start.split("T", 1)[0]
+        dt_end = end.split("T", 1)[0]
+        date_index = pd.date_range(start=dt_start, end=dt_end)
+    return date_index
+
+
+def package_landfastice_data(landfastice_resp):
+    """Package landfast ice extent data into a nested JSON-like dict.
+
+    Arguments:
+        time_index (pandas DateRange object) -- a time index with daily frequency
+        landfastice_resp (list) -- the response from the WCS GetCoverage request
+
+    Returns:
+        di (dict) -- a dict where the key is a single date and the value is the landfast ice status (1 or 0)
+    """
+    time_index = generate_time_index("all")
+    di = {}
+    for t, x in zip(list(time_index), landfastice_resp):
+        di[t.date().strftime("%m-%d-%Y")] = x
+    return di
+
+
+@routes.route("/landfastice/")
+@routes.route("/landfastice/abstract/")
+def about_landfastice():
+    return render_template("landfastice/abstract.html")
+
+
+@routes.route("/landfastice/point/")
+def about_landfastice_point():
+    return render_template("landfastice/point.html")
+
+
+@routes.route("/landfastice/point/<lat>/<lon>/")
+def run_point_fetch_all_landfastice(lat, lon):
+    """Run the async request for landfast ice extent data at a single point.
+    Args:
+        lat (float): latitude
+        lon (float): longitude
+
+    Returns:
+        JSON-like dict of landfast ice extent data
+    """
+    validation = validate_latlon(lat, lon)
+    if validation == 400:
+        return render_template("400/bad_request.html"), 400
+    if validation == 422:
+        return (
+            render_template(
+                "422/invalid_latlon.html", west_bbox=WEST_BBOX, east_bbox=EAST_BBOX
+            ),
+            422,
+        )
+    x, y = project_latlon(lat, lon, 3338)
+    try:
+        rasdaman_response = asyncio.run(
+            fetch_wcs_point_data(x, y, landfastice_coverage_id)
+        )
+        landfastice_time_series = package_landfastice_data(rasdaman_response)
+
+        # landfastice_conc = postprocess(
+        #     package_landfastice_data(rasdaman_response), "landfastice"
+        # )
+        # if request.args.get("format") == "csv":
+        #     if type(landfastice_conc) is not dict:
+        #         # Returns errors if any are generated
+        #         return landfastice_conc
+        #     # Returns CSV for download
+        #     return create_csv(
+        #         postprocess(package_landfastice_data(rasdaman_response), "landfastice"),
+        #         lat,
+        #         lon,
+        #     )
+        # # Returns sea ice concentrations across years & months
+        # return postprocess(package_landfastice_data(rasdaman_response), "landfastice")
+        # return landfast_time_index
+        # return package_landfastice_data(rasdaman_response)
+        return landfastice_time_series
+    except Exception as exc:
+        if hasattr(exc, "status") and exc.status == 404:
+            return render_template("404/no_data.html"), 404
+        return render_template("500/server_error.html"), 500
