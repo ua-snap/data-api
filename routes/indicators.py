d9329fb71d6a90c9a5e12e9c03d52ae676fd3b70
kyleredilla <kmredilla@alaska.edu>
2023-05-08 12:10:57 -0800

    add packaging functionality for tx above/below endpoint

--- routes/indicators.py
+++ routes/indicators.py
@@ -12,25 +12,14 @@ from shapely.geometry import Point
 # local imports
 from generate_urls import generate_wcs_query_url
 from fetch_data import *
-from validate_request import (
-    validate_latlon,
-    project_latlon,
-    validate_var_id,
-    validate_year,
-)
-from validate_data import (
-    get_poly_3338_bbox,
-    nullify_and_prune,
-    postprocess,
-    place_name_and_type,
-)
+from validate_request import validate_latlon
 from . import routes
 from config import WEST_BBOX, EAST_BBOX
 
 indicators_api = Blueprint("indicators_api", __name__)
 
 # All of the cordex_<variable name> coverages should have the same encodings 
-cordex_dim_encodings = asyncio.run(get_dim_encodings("cordex_tas"))
+dim_encodings = asyncio.run(get_dim_encodings("cordex_tas"))
 
 cordex_indicator_coverage_lu = {
     "tx_days_above": "cordex_tasmax",
@@ -41,13 +30,12 @@ def generate_tx_days_above_or_below_wcps_str(cov_id, tx, lat, lon, start_year, n
     """WCPS query to count the number of days in above or below a given threshold. Assumes coverage has scenario and model axes and iterates over all combinations.
     
     Args:
-        cov_di (str): coverage ID
+        cov_id (str): coverage ID
         tx (float/int): threshold value
         lat (float): latitude coordinate for point query, or string composed as "lower:upper" for bbox query, where lower and upper are lower and upper bounds of bbox
         lon (float): longitude coordinate for point query, or string composed as "lower:upper" for bbox query, where lower and upper are lower and upper bounds of bbox
         start_year (int): Starting year of era to summarize over
         n_years (int): number of consecutive years of era to summarize over
-        
         tx_above (bool): count days above threshold tx. Set to False to count days below tx
         encoding (str): currently supports either "json" or "netcdf" for point or bbox queries, respectively
         
@@ -66,6 +54,7 @@ def generate_tx_days_above_or_below_wcps_str(cov_id, tx, lat, lon, start_year, n
             f"ProcessCoverages&query=for $c in ({cov_id}) "
             "return encode("
                 "coverage tx_days_above_or_below "
+                # <axis_name>(domain($c, <axis_name>)) specifies iteration over all available coordinates
                 f"over $s scenario(domain($c, scenario)), $m model(domain($c, model)), $y year(0:{n_years - 1}) "
                 "values count("
                     f"$c[scenario($s), model($m), lat({lat}), lon({lon}), "
@@ -74,7 +63,7 @@ def generate_tx_days_above_or_below_wcps_str(cov_id, tx, lat, lon, start_year, n
                             f"imageCrsDomain($c[ansi({ansi_str})], ansi).hi + ($y * 365)"
                         ")"
                     f"] - 273.15 {operand} {tx}"
-                f'), "application/{encoding}"'
+                f'), "application/{encoding}", "nodata=nan"'
             ")"
         )
     )
@@ -112,12 +101,36 @@ async def fetch_yearly_tx_above_or_below_point_data(indicator_id, **kwargs):
     return point_data_list
 
 
+def package_era_indicator_results(point_data_list):
+    """Package the indicator values for a given query (assumed for a given year range)
+    
+    Args:
+        point_data_list (list): nested list of data from Rasdaman WCPS query
+
+    Returns:
+        di (dict): dictionary mirroring structure of nested list with keys derived from dim_encodings global variable
+    """
+    di = dict()
+    for si, mod_li in enumerate(point_data_list):
+        scenario = dim_encodings["scenario"][si]
+        di[scenario] = dict()
+        for mi, yr_li in enumerate(mod_li):
+            model = dim_encodings["model"][mi]
+            yr_li = np.array(yr_li).astype(float)
+            di[scenario][model] = dict()
+            di[scenario][model]["min"] = np.min(yr_li).round(1)
+            di[scenario][model]["mean"] = np.mean(yr_li).round(1)
+            di[scenario][model]["max"] = np.max(yr_li).round(1)
+    
+    return di
+
+
 @routes.route("/indicators/tx_days_above/<tx>/point/<lat>/<lon>")
 def run_fetch_tx_days_above_point_data(tx, lat, lon):
     """
 
     Args:
-        
+        tx (float): temeprature threshold value
         lat (float): latitude
         lon (float): longitude
 
@@ -141,15 +154,21 @@ def run_fetch_tx_days_above_point_data(tx, lat, lon):
     try:
         # TO-DO: maybe have a if clause here to check if threshold is within reasonable range?
         _ = float(tx)
-        # try:
-        point_data_list = asyncio.run(
-            fetch_yearly_tx_above_or_below_point_data(
-                "tx_days_above", tx=tx, lat=lat, lon=lon, start_year=2006, n_years=30, encoding="json", above=True
+        n_years = 30
+        start_years = [2010, 2040, 2070]
+        indicator_package = {}
+        for year in start_years:
+
+            point_data_list = asyncio.run(
+                fetch_yearly_tx_above_or_below_point_data(
+                    "tx_days_above", tx=tx, lat=lat, lon=lon, start_year=year, n_years=n_years, encoding="json", above=True
+                )
             )
-        )
-            
+            era_key = f"{year}-{year + n_years - 1}"
+            indicator_package[era_key] = package_era_indicator_results(point_data_list)
+
     except ValueError:
         return render_template("400/bad_request.html"), 400
 
-    return point_data_list
+    return indicator_package
 
