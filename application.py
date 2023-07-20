17e624cddb911127eb403490b888dff433710617
Bob Torgerson <rltorgerson@alaska.edu>
2023-07-20 14:26:21 -0800

    Silences warnings in the console window.

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 from flask import Flask, render_template
 from flask_cors import CORS
+import warnings
+
+# Silence warnings in console before importing routes
+warnings.filterwarnings("ignore")
 
 from routes import *
 
