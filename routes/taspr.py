53e96624fbb4b2e56f365ca87beb36f1193a50cf
Craig Stephenson <crstephenson@alaska.edu>
2023-10-04 10:41:58 -0800

    Rename and change route for proj_precip and update documentation (#336)
    * Rename and change route to proj_precip and update documentation.
    
    * + explicit var validation, + 2022 DOT precip ref
    
    ---------
    
    Co-authored-by: jdpaul3 <jdpaul3@atlas10.snap.uaf.edu>

--- routes/taspr.py
+++ routes/taspr.py
@@ -1223,6 +1223,8 @@ def point_data_endpoint(var_ep, lat, lon):
             if hasattr(exc, "status") and exc.status == 404:
                 return render_template("404/no_data.html"), 404
             return render_template("500/server_error.html"), 500
+    else:
+        return render_template("400/bad_request.html"), 400
 
     if request.args.get("format") == "csv":
         point_pkg = nullify_and_prune(point_pkg, "taspr")
@@ -1260,6 +1262,9 @@ def taspr_area_data_endpoint(var_ep, var_id):
             poly_pkg = run_aggregate_var_polygon(var_ep, var_id)
         elif var_ep == "taspr":
             poly_pkg = run_aggregate_allvar_polygon(var_id)
+        else:
+            return render_template("400/bad_request.html"), 400
+
     except:
         return render_template("422/invalid_area.html"), 422
 
@@ -1272,7 +1277,7 @@ def taspr_area_data_endpoint(var_ep, var_id):
     return postprocess(poly_pkg, "taspr")
 
 
-@routes.route("/proj_precip/point/<lat>/<lon>")
+@routes.route("/precipitation/frequency/point/<lat>/<lon>")
 def proj_precip_point(lat, lon):
     validation = validate_latlon(lat, lon)
     if validation == 400:
