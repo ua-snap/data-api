41c113683adb9441ec482536d6ed61f32e791030
Craig Stephenson <crstephenson@alaska.edu>
2023-10-26 11:36:55 -0800

    Incorporate more types of Yukon areas.

--- fetch_data.py
+++ fetch_data.py
@@ -152,7 +152,7 @@ def get_poly_3338_bbox(poly_id, crs=3338):
             fetch_data(
                 [
                     generate_wfs_places_url(
-                        "all_boundaries:all_areas", "the_geom", poly_id, "id"
+                        "playground:all_areas_test", "the_geom", poly_id, "id"
                     )
                 ]
             )
