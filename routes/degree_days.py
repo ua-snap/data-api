0e6c4edd8cb31b7a70bffaa3ab0c600b9121c008
Charles Parr <cparr4@alaska.edu>
2024-04-08 08:25:18 -0800

    package mmm query response

--- routes/degree_days.py
+++ routes/degree_days.py
@@ -51,13 +51,38 @@ n_results_lu = {
 }
 
 
+def validate_years(start_year, end_year):
+    """Check provided years against valid ranges for historical vs. projected.
+
+    Args:
+        start_year (int): start year for WCPS query or None
+        end_year (int): end year for WCPS query or None
+
+    Returns:
+        True if years are valid, otherwise an error page to show valid years
+    """
+    if None not in [start_year, end_year]:
+        min_year = years_lu["historical"]["min"]
+        max_year = years_lu["projected"]["max"]
+
+        for year in [start_year, end_year]:
+            if year < min_year or year > max_year:
+                return (
+                    render_template(
+                        "422/invalid_year.html",
+                        start_year=start_year,
+                        end_year=end_year,
+                        min_year=min_year,
+                        max_year=max_year,
+                    ),
+                    422,
+                )
+    return True
+
+
 def make_time_slicer(min_year, max_year):
     time_slicer = f"{min_year}:{max_year}"
     return time_slicer
-
-
-def make_wcps_slicers(start_year, end_year):
-    pass
 
 
 def get_dd_wcps_request_str(
@@ -71,7 +96,7 @@ def get_dd_wcps_request_str(
     num_results,
     encoding="json",
 ):
-    """Generate WCPS query fragment specific to the degree days min-mean-max summarization.
+    """Generate WCPS query fragment specific to degree days min-mean-max summarization.
 
     Args:
         x (float or str): x-coordinate for point query, or string
@@ -112,7 +137,60 @@ def get_dd_wcps_request_str(
     return wcps_str
 
 
-# CP note: called by run_fetch...then point package. basic route
+def package_unabridged_response(nested_list, start_year=None, end_year=None):
+    """Convert nested list of unabridged degree days data to JSON-like dict."""
+
+    unabridged = {}
+
+    for model_index, model_data in enumerate(nested_list):
+        model_name = dd_dim_encodings["model"][model_index]
+        unabridged[model_name] = {}
+
+        for scenario_index, scenario_data in enumerate(model_data):
+            scenario_name = dd_dim_encodings["scenario"][scenario_index]
+            unabridged[model_name][scenario_name] = {}
+
+            for year_index, dd_value in enumerate(scenario_data):
+                year = 1950 + year_index
+                unabridged[model_name][scenario_name][year] = dd_value
+
+    return unabridged
+
+
+def package_distilled_response(mmm_point_data, start_year=None, end_year=None):
+    """Package min-mean-max degree days to labeled JSON-like dict."""
+    if request.args.get("summarize") == "mmm":
+        distilled = {}
+        distilled["historical"] = {}
+        distilled["projected"] = {}
+
+        distilled["historical"]["ddmin"] = mmm_point_data["min"]["historical"][
+            "wcps_response"
+        ]
+        distilled["historical"]["ddmean"] = round(
+            mmm_point_data["mean"]["historical"]["wcps_response"]
+        )
+        distilled["historical"]["ddmax"] = mmm_point_data["max"]["historical"][
+            "wcps_response"
+        ]
+
+        distilled["projected"]["ddmin"] = mmm_point_data["min"]["projected"][
+            "wcps_response"
+        ]
+        distilled["projected"]["ddmean"] = round(
+            mmm_point_data["mean"]["projected"]["wcps_response"]
+        )
+        distilled["projected"]["ddmax"] = mmm_point_data["max"]["projected"][
+            "wcps_response"
+        ]
+        return distilled
+
+    else:
+        # CP note: function should only get called when request is mmm
+        # so let's fail if this function gets called otherwise
+        assert request.args.get("summarize") == "mmm"
+
+
 async def fetch_dd_point_data(x, y, cov_id, start_year=None, end_year=None):
     """Run the async degree days data request for a single point.
 
@@ -193,105 +271,20 @@ async def fetch_dd_point_data(x, y, cov_id, start_year=None, end_year=None):
                         stat_function,
                         n_results_lu[era],
                     )
-
+                    # CP note: print below for helpful debugging
                     wcps_url = generate_wcs_query_url(wcps_str)
-                    print(wcps_url)
                     mmm_dispatch[stat_function][era]["wcps_url"] = wcps_url
 
-        # mmm_dispatch[stat_function][era]["wcps_response"] = (
-        #     await fetch_data([mmm_dispatch[stat_function][era]["wcps_url"]])
-        # )
-
-        # point_data_list.append(
-        #     mmm_dispatch[stat_function][era]["wcps_response"]
-        # )
+                    mmm_dispatch[stat_function][era]["wcps_response"] = (
+                        await fetch_data([wcps_url])
+                    )
+        return mmm_dispatch
     else:
         request_str = generate_wcs_getcov_str(x, y, cov_id)
         wcs_query_url = generate_wcs_query_url(request_str)
         point_data_list = await fetch_data([wcs_query_url])
 
-    return point_data_list
-
-
-def package_unabridged_response(nested_list, start_year=None, end_year=None):
-    """Convert nested list of unabridged degree days data to JSON-like dict."""
-
-    unabridged = {}
-
-    for model_index, model_data in enumerate(nested_list):
-        model_name = dd_dim_encodings["model"][model_index]
-        unabridged[model_name] = {}
-
-        for scenario_index, scenario_data in enumerate(model_data):
-            scenario_name = dd_dim_encodings["scenario"][scenario_index]
-            unabridged[model_name][scenario_name] = {}
-
-            for year_index, dd_value in enumerate(scenario_data):
-                year = 1950 + year_index
-                unabridged[model_name][scenario_name][year] = dd_value
-
-    return unabridged
-
-
-def package_distilled_response(point_data, start_year=None, end_year=None):
-    """Convert nested list of distilled (mean, min, max) degree days data to JSON-like dict."""
-
-    distilled = {}
-    if request.args.get("summarize") == "mmm":
-        # need to see how WCPS request changes, will pick up edits here
-        distilled["historical"] = {}
-        distilled["projected"] = {}
-
-        historical_max = round(point_data[0], 1)
-        historical_mean = round(point_data[1], 1)
-        historical_min = round(point_data[2], 1)
-        distilled["historical"]["ddmin"] = round(historical_min)
-        distilled["historical"]["ddmean"] = round(historical_mean)
-        distilled["historical"]["ddmax"] = round(historical_max)
-        projected_max = round(point_data[3], 1)
-        projected_mean = round(point_data[4], 1)
-        projected_min = round(point_data[5], 1)
-        distilled["projected"]["ddmin"] = round(projected_min)
-        distilled["projected"]["ddmean"] = round(projected_mean)
-        distilled["projected"]["ddmax"] = round(projected_max)
-    else:
-        # CP note: this function should only get called when the summary param is mmm
-        # so let's fail if this function gets called otherwise
-        assert request.args.get("summarize") == "mmm"
-    # else:
-    #     for mi, v_li in enumerate(point_data):  # (nested list with model at dim 0)
-    #         if mi == 0:
-    #             min_year = years_lu["historical"]["min"]
-    #             max_year = years_lu["historical"]["max"]
-    #             years = range(min_year, max_year + 1)
-    #         else:
-    #             min_year = years_lu["projected"]["min"]
-    #             max_year = years_lu["projected"]["max"]
-    #             years = range(min_year, max_year + 1)
-
-    #         model = dd_dim_encodings["model"][mi]
-    #         point_pkg[model] = {}
-
-    # Responses from Rasdaman include the same array length for both
-    # historical and projected data, representing every possible year
-    # (1979-2100). This means both the historical and projected data
-    # arrays include nodata years populated with 0s. The code below
-    # omits nodata gaps and makes sure the correct year is assigned to
-    # its corresponding data in the historical and projected data
-    # arrays.
-    # year = years_lu["historical"]["min"]
-    # year_index = 0
-    # if None in [start_year, end_year]:
-    #     start_year = years_lu["historical"]["min"]
-    #     end_year = years_lu["projected"]["max"]
-    # for value in v_li:
-    #     if year in years:
-    #         if year >= int(start_year) and year <= int(end_year):
-    #             point_pkg[model][years[year_index]] = {"dd": round(value)}
-    #         year_index += 1
-    #     year += 1
-
-    return distilled
+        return point_data_list
 
 
 @routes.route("/degree_days/")
@@ -302,6 +295,75 @@ def package_distilled_response(point_data, start_year=None, end_year=None):
 @routes.route("/degree_days/freezing_index/")
 def degree_days_about():
     return render_template("/documentation/degree_days.html")
+
+
+@routes.route("/degree_days/<var_ep>/<lat>/<lon>")
+@routes.route("/degree_days/<var_ep>/<lat>/<lon>/<start_year>/<end_year>")
+def run_fetch_dd_point_data(
+    var_ep, lat, lon, start_year=None, end_year=None, preview=None
+):
+    """Fetch degree days point data for specified lat/lon and return JSON-like dict.
+
+    Args:
+        var_ep (str): heating, below_zero, thawing_index, or freezing_index
+        lat (float): latitude
+        lon (float): longitude
+        start_year (int): optional start year for WCPS query
+        end_year (int): optional end year for WCPS query
+
+    Returns:
+        JSON-like dict of requested degree days data
+    """
+    validation = validate_latlon(lat, lon)
+    if validation == 400:
+        return render_template("400/bad_request.html"), 400
+    if validation == 422:
+        return (
+            render_template(
+                "422/invalid_latlon.html", west_bbox=WEST_BBOX, east_bbox=EAST_BBOX
+            ),
+            422,
+        )
+    x, y = project_latlon(lat, lon, 3338)
+
+    if None not in [start_year, end_year]:
+        valid_year = validate_years(int(start_year), int(end_year))
+        if valid_year is not True:
+            return valid_year
+
+    if var_ep in var_ep_lu.keys():
+        cov_id_str = var_ep_lu[var_ep]["cov_id_str"]
+        try:
+            point_data_list = asyncio.run(
+                fetch_dd_point_data(x, y, cov_id_str, start_year, end_year)
+            )
+        except Exception as exc:
+            if hasattr(exc, "status") and exc.status == 404:
+                return render_template("404/no_data.html"), 404
+            return render_template("500/server_error.html"), 500
+    else:
+        return render_template("400/bad_request.html"), 400
+
+    if request.args.get("summarize") == "mmm":
+        dd_data_package = package_distilled_response(
+            point_data_list, start_year, end_year
+        )
+    else:
+        dd_data_package = package_unabridged_response(
+            point_data_list, start_year, end_year
+        )
+
+    # if request.args.get("format") == "csv" or preview:
+    #     point_pkg = nullify_and_prune(point_pkg, cov_id_str)
+    #     if point_pkg in [{}, None, 0]:
+    #         return render_template("404/no_data.html"), 404
+    #     if request.args.get("summarize") == "mmm":
+    #         return create_csv(point_pkg, cov_id_str, lat=lat, lon=lon)
+    #     else:
+    #         return create_csv(point_pkg, cov_id_str + "_all", lat=lat, lon=lon)
+
+    # return postprocess(point_pkg, cov_id_str)
+    return dd_data_package
 
 
 @routes.route("/eds/degree_days/<var_ep>/<lat>/<lon>")
@@ -375,103 +437,3 @@ def get_dd_plate(var_ep, lat, lon):
     dd["preview"] = first + last
 
     return jsonify(dd)
-
-
-@routes.route("/degree_days/<var_ep>/<lat>/<lon>")
-@routes.route("/degree_days/<var_ep>/<lat>/<lon>/<start_year>/<end_year>")
-def run_fetch_dd_point_data(
-    var_ep, lat, lon, start_year=None, end_year=None, preview=None
-):
-    """Degree days data endpoint. Fetch point data for
-    specified lat/lon and return JSON-like dict.
-
-    Args:
-        var_ep (str): heating, below_zero, thawing_index, or freezing_index
-        lat (float): latitude
-        lon (float): longitude
-        start_year (int): optional start year for WCPS query
-        end_year (int): optional end year for WCPS query
-
-    Returns:
-        JSON-like dict of requested degree days data
-
-    Notes:
-        example request: http://localhost:5000/degree_days/heating/all/65/-147
-    """
-    validation = validate_latlon(lat, lon)
-    if validation == 400:
-        return render_template("400/bad_request.html"), 400
-    if validation == 422:
-        return (
-            render_template(
-                "422/invalid_latlon.html", west_bbox=WEST_BBOX, east_bbox=EAST_BBOX
-            ),
-            422,
-        )
-
-    x, y = project_latlon(lat, lon, 3338)
-
-    if None not in [start_year, end_year]:
-        valid_year = validate_years(int(start_year), int(end_year))
-        if valid_year is not True:
-            return valid_year
-
-    if var_ep in var_ep_lu.keys():
-        cov_id_str = var_ep_lu[var_ep]["cov_id_str"]
-        try:
-            point_data_list = asyncio.run(
-                fetch_dd_point_data(x, y, cov_id_str, start_year, end_year)
-            )
-        except Exception as exc:
-            if hasattr(exc, "status") and exc.status == 404:
-                return render_template("404/no_data.html"), 404
-            return render_template("500/server_error.html"), 500
-    else:
-        return render_template("400/bad_request.html"), 400
-
-    # point_pkg = package_dd_point_data(point_data_list, start_year, end_year)
-    unabridged_package = package_unabridged_response(
-        point_data_list, start_year, end_year
-    )
-
-    # if request.args.get("format") == "csv" or preview:
-    #     point_pkg = nullify_and_prune(point_pkg, cov_id_str)
-    #     if point_pkg in [{}, None, 0]:
-    #         return render_template("404/no_data.html"), 404
-    #     if request.args.get("summarize") == "mmm":
-    #         return create_csv(point_pkg, cov_id_str, lat=lat, lon=lon)
-    #     else:
-    #         return create_csv(point_pkg, cov_id_str + "_all", lat=lat, lon=lon)
-
-    # return postprocess(point_pkg, cov_id_str)
-    return unabridged_package
-    # return {"a": 3442}
-
-
-def validate_years(start_year, end_year):
-    """Check provided years against valid ranges for historical vs. projected.
-
-    Args:
-        start_year (int): start year for WCPS query or None
-        end_year (int): end year for WCPS query or None
-
-    Returns:
-        True if years are valid, otherwise an error page to show valid years
-    """
-    if None not in [start_year, end_year]:
-        min_year = years_lu["historical"]["min"]
-        max_year = years_lu["projected"]["max"]
-
-        for year in [start_year, end_year]:
-            if year < min_year or year > max_year:
-                return (
-                    render_template(
-                        "422/invalid_year.html",
-                        start_year=start_year,
-                        end_year=end_year,
-                        min_year=min_year,
-                        max_year=max_year,
-                    ),
-                    422,
-                )
-    return True
