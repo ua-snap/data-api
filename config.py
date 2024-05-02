cadaaee5b70e067a5a9183bbb37be8d374e909d6
Craig Stephenson <crstephenson@alaska.edu>
2024-05-02 13:18:14 -0800

    Make sure SITE_OFFLINE environment variable exists before attempting to lowercase it.

--- config.py
+++ config.py
@@ -10,4 +10,8 @@ WEST_BBOX = [-180, 51.3492, -122.8098, 71.3694]
 EAST_BBOX = [172.4201, 51.3492, 180, 71.3694]
 SEAICE_BBOX = [-180, 30.98, 180, 90]
 WEB_APP_URL = os.getenv("WEB_APP_URL") or "https://northernclimatereports.org/"
-SITE_OFFLINE = os.getenv("SITE_OFFLINE").lower() == "true" or False
+
+if os.getenv("SITE_OFFLINE"):
+    SITE_OFFLINE = os.getenv("SITE_OFFLINE").lower() == "true"
+else:
+    SITE_OFFLINE = False
