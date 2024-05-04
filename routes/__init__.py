b9cea77824c96eb039b605dabbd9b45c2d2e6035
Bob Torgerson <rltorgerson@alaska.edu>
2024-05-03 20:14:41 -0800

    Adds a decorator to all routes that redirects to the root of the site ('/') if SITE_OFFLINE is True. (#441)

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,6 +1,20 @@
-from flask import Blueprint
+import os
+from flask import Blueprint, redirect
+from config import SITE_OFFLINE
 
 routes = Blueprint("routes", __name__)
+
+
+def check_site_offline():
+    if SITE_OFFLINE:
+        return redirect("/")
+
+
+# Applies a decorator to all routes to check for the SITE_OFFLINE environment variable.
+@routes.before_request
+def enforce_site_offline():
+    return check_site_offline()
+
 
 from .fire import *
 from .permafrost import *
