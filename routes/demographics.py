e526921cdd7aa8cb3e3c9ff9a7ae81b856f4d48e
Joshdpaul <jdpaul3@alaska.edu>
2024-06-04 13:00:42 -0800

    add option for all communities

--- routes/demographics.py
+++ routes/demographics.py
@@ -16,14 +16,18 @@ def validate_community_id(community):
     Args:
            community (string): A community ID from route input
     Returns:
-            Boolean : True means the community ID is valid, False means it is not valid
+            Tuple of boolean and list: for boolean, True means the community ID is valid and False means it is not valid; list is all valid community IDs
     """
-    url = generate_wfs_places_url("demographics:demographics", filter=community, filter_type="id")
+    #url = generate_wfs_places_url("demographics:demographics", filter=community, filter_type="id")
+    community_ids = []
+    url = generate_wfs_places_url("demographics:demographics", properties="id")
     with requests.get(url) as r:
-        if r.json()['features'] == []:
-            return False
-        else: return True
-
+        for feature in r.json()['features']:
+            community_ids.append(feature['properties']['id'])
+    if community in community_ids:
+        return True, community_ids
+    else: 
+        return False, community_ids
 
 @routes.route("/demographics/")
 def demographics_about():
@@ -34,7 +38,7 @@ def get_data_for_community(community):
     """
     Function to pull demographics data as JSON or CSV.
        Args:
-           community (string): A community ID from https://earthmaps.io/places/communities.
+           community (string): A community ID from https://earthmaps.io/places/communities, or "all" to get all available communities
 
        Returns:
            JSON-formatted output of demographic data for the requested community,
@@ -44,12 +48,16 @@ def get_data_for_community(community):
            example: http://localhost:5000/demographics/AK15
     """
     # Validate community ID; if not valid, return an error
-    if not validate_community_id(community):
+    validation, community_ids = validate_community_id(community)
+    if community == 'all': pass
+    elif not validation:
         return render_template("400/bad_request.html"), 400
+    else:
+        community_ids = [community, "US0", "AK0"]
 
     # List URLs
     urls = []
-    for c in [community, "US0", "AK0"]:
+    for c in community_ids:
         urls.append(generate_wfs_places_url("demographics:demographics", filter=c, filter_type="id"))
 
     # Requests the Geoserver WFS URLs and extracts property values to a dict
@@ -58,7 +66,7 @@ def get_data_for_community(community):
         results[r["features"][0]["properties"]["id"]] = r["features"][0]["properties"]
 
     # Rename keys
-    for c in [community, "US0", "AK0"]:
+    for c in community_ids:
         fields_to_rename = [x for x in list(results[c].keys()) if x in list(demographics_fields.keys())]
         for field in fields_to_rename:
             results[c][demographics_fields[field]] = results[c].pop(field)
@@ -73,7 +81,7 @@ def get_data_for_community(community):
     ]
 
     reformatted_results = {}
-    for c in [community, "US0", "AK0"]:
+    for c in community_ids:
         reformatted_results[c] = {}
         for field in fields:
             reformatted_results[c][field] = results[c][field]
