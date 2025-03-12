bd2808e8143bdd8e0ba6c67de5b26491c9332bf9
joshdpaul <jdpaul3@alaska.edu>
2025-03-12 09:01:50 -0800

    get beetles dim encodings direct from rasdaman, identify code to replace with new functions

--- routes/beetles.py
+++ routes/beetles.py
@@ -8,7 +8,8 @@ from generate_requests import generate_netcdf_wcs_getcov_str
 from fetch_data import (
     fetch_bbox_netcdf_list,
     fetch_wcs_point_data,
-    zonal_stats,
+    describe_via_wcps,
+    # zonal_stats,
     itertools,
     get_poly,
 )
@@ -17,37 +18,49 @@ from validate_request import (
     validate_latlon,
     project_latlon,
     validate_var_id,
+    get_coverage_encodings,
 )
 from postprocessing import nullify_and_prune, postprocess
 from . import routes
 from config import WEST_BBOX, EAST_BBOX
 
 beetles_api = Blueprint("beetles_api", __name__)
-# Rasdaman targets
-beetle_coverage_id = "beetle_risk"
 
-dim_encodings = {
-    "model": {
-        0: "NCAR-CCSM4",
-        1: "GFDL-ESM2M",
-        2: "HadGEM2-ES",
-        3: "MRI-CGCM3",
+var_ep_lu = {
+    "beetles": {
+        "cov_id_str": "beetle_risk",
+        "dim_encodings": None,  # populated below
+        "bandname": "Gray",
+        "label": None,
     },
-    "scenario": {
-        0: "rcp45",
-        1: "rcp85",
-    },
-    "era": {
-        0: "2010-2039",
-        1: "2040-2069",
-        2: "2070-2099",
-    },
-    "snowpack": {0: "low", 1: "medium"},
-    # The 0 in climate_protection represents NO DATA in this context,
-    # but needs to remain as 0 to allow for the pruning function
-    # to correctly identify it as unrepresented space in the model.
-    "climate_protection": {0: 0, 1: "high", 2: "minimal", 3: "none"},
 }
+
+var_ep_lu["dim_encodings"] = (
+    get_coverage_encodings(describe_via_wcps(var_ep_lu["beetles"]["cov_id_str"])),
+)
+
+# dim_encodings = {
+#     "model": {
+#         0: "NCAR-CCSM4",
+#         1: "GFDL-ESM2M",
+#         2: "HadGEM2-ES",
+#         3: "MRI-CGCM3",
+#     },
+#     "scenario": {
+#         0: "rcp45",
+#         1: "rcp85",
+#     },
+#     "era": {
+#         0: "2010-2039",
+#         1: "2040-2069",
+#         2: "2070-2099",
+#     },
+#     "snowpack": {0: "low", 1: "medium"},
+#     # The 0 in climate_protection represents NO DATA in this context,
+#     # but needs to remain as 0 to allow for the pruning function
+#     # to correctly identify it as unrepresented space in the model.
+#     "climate_protection": {0: 0, 1: "high", 2: "minimal", 3: "none"},
+# }
 
 
 async def fetch_beetles_bbox_data(bbox_bounds, cov_id_str):
@@ -66,7 +79,10 @@ async def fetch_beetles_bbox_data(bbox_bounds, cov_id_str):
     request_strs.append(generate_netcdf_wcs_getcov_str(bbox_bounds, cov_id_str))
     urls = [generate_wcs_query_url(request_str) for request_str in request_strs]
     bbox_ds_list = await fetch_bbox_netcdf_list(urls)
-    return bbox_ds_list
+    bbox_ds = bbox_ds_list[
+        0
+    ]  # # there is only ever one dataset in the list for this endpoint
+    return bbox_ds
 
 
 def package_beetle_data(beetle_resp, beetle_percents=None):
@@ -78,6 +94,8 @@ def package_beetle_data(beetle_resp, beetle_percents=None):
     Returns:
         di -- a nested dictionary of all beetle risk values
     """
+    dim_encodings = var_ep_lu["dim_encodings"]
+
     # initialize the output dict
     di = dict()
 
@@ -174,153 +192,153 @@ def package_beetle_data(beetle_resp, beetle_percents=None):
     return di
 
 
-def summarize_within_poly_marr(ds, poly_mask_arr, bandname="Gray"):
-    """Summarize a single Data Variable of a xarray.DataSet within a polygon.
-    Return the results as a nested dict.
+# def summarize_within_poly_marr(ds, poly_mask_arr, bandname="Gray"):
+#     """Summarize a single Data Variable of a xarray.DataSet within a polygon.
+#     Return the results as a nested dict.
 
-    Args:
-        ds (xarray.DataSet): DataSet with "Gray" as variable of
-            interest
-        poly_mask_arr (numpy.ma.core.MaskedArray): a masked array masking the cells intersecting
-            the polygon of interest
-        dim_encodings (dict): nested dictionary of thematic key value pairs that characterize the
-            data and map integer data coordinates to models, scenarios, eras, snowpacks, etc.
-        bandname (str): name of variable in ds, defaults to "Gray" for Rasdaman coverages where
-            the name is not given at ingest
+#     Args:
+#         ds (xarray.DataSet): DataSet with "Gray" as variable of
+#             interest
+#         poly_mask_arr (numpy.ma.core.MaskedArray): a masked array masking the cells intersecting
+#             the polygon of interest
+#         dim_encodings (dict): nested dictionary of thematic key value pairs that characterize the
+#             data and map integer data coordinates to models, scenarios, eras, snowpacks, etc.
+#         bandname (str): name of variable in ds, defaults to "Gray" for Rasdaman coverages where
+#             the name is not given at ingest
 
-    Returns:
-        Nested dict of results for all non-X/Y axis combinations,
-    """
+#     Returns:
+#         Nested dict of results for all non-X/Y axis combinations,
+#     """
 
-    # operates on underlying DataArray
-    da = ds[bandname]
+#     # operates on underlying DataArray
+#     da = ds[bandname]
 
-    # get axis (dimension) names and make list of all coordinate combinations
-    all_dims = da.dims
-    dimnames = [dimname for dimname in all_dims if dimname not in ("X", "Y")]
-    iter_coords = list(
-        itertools.product(*[list(ds[dimname].values) for dimname in dimnames])
-    )
+#     # get axis (dimension) names and make list of all coordinate combinations
+#     all_dims = da.dims
+#     dimnames = [dimname for dimname in all_dims if dimname not in ("X", "Y")]
+#     iter_coords = list(
+#         itertools.product(*[list(ds[dimname].values) for dimname in dimnames])
+#     )
 
-    data_arr = []
-    for coords in iter_coords:
-        sel_di = {dimname: int(coord) for dimname, coord in zip(dimnames, coords)}
-        data_arr.append(da.sel(sel_di).values)
-    data_arr = np.array(data_arr)
+#     data_arr = []
+#     for coords in iter_coords:
+#         sel_di = {dimname: int(coord) for dimname, coord in zip(dimnames, coords)}
+#         data_arr.append(da.sel(sel_di).values)
+#     data_arr = np.array(data_arr)
 
-    # need to transpose the 2D spatial slices if X is the "rows" dimension
-    if all_dims.index("X") < all_dims.index("Y"):
-        data_arr = data_arr.transpose(0, 2, 1)
+#     # need to transpose the 2D spatial slices if X is the "rows" dimension
+#     if all_dims.index("X") < all_dims.index("Y"):
+#         data_arr = data_arr.transpose(0, 2, 1)
 
-    data_arr_mask = np.broadcast_to(poly_mask_arr.mask, data_arr.shape)
-    data_arr[data_arr_mask] = np.nan
+#     data_arr_mask = np.broadcast_to(poly_mask_arr.mask, data_arr.shape)
+#     data_arr[data_arr_mask] = np.nan
 
-    # Adds one to each value to generate correct shape and
-    # iterates through the data below.
-    eras = sel_di["era"] + 1
-    models = sel_di["model"] + 1
-    scenarios = sel_di["scenario"] + 1
-    snowpacks = sel_di["snowpack"] + 1
+#     # Adds one to each value to generate correct shape and
+#     # iterates through the data below.
+#     eras = sel_di["era"] + 1
+#     models = sel_di["model"] + 1
+#     scenarios = sel_di["scenario"] + 1
+#     snowpacks = sel_di["snowpack"] + 1
 
-    return_arr = np.zeros((eras, models, scenarios, snowpacks))
+#     return_arr = np.zeros((eras, models, scenarios, snowpacks))
 
-    # Percentages of each slice of the data. Has a last dimension that are
-    # integer values representing 1 = low risk, 2 = moderate risk, 3 = high risk
-    return_percentages = np.zeros((eras, models, scenarios, snowpacks, 4))
-    for era in range(eras):
-        for model in range(models):
-            for scenario in range(scenarios):
-                for snowpack in range(snowpacks):
-                    # Generates an index out of the current iteration
-                    # for use with the data_arr that has a flat shape
-                    # that matches the below code.
-                    index = (
-                        (era * models * scenarios * snowpacks)
-                        + (model * scenarios * snowpacks)
-                        + (scenario * snowpacks)
-                        + snowpack
-                    )
-                    slice = data_arr[index]
+#     # Percentages of each slice of the data. Has a last dimension that are
+#     # integer values representing 1 = low risk, 2 = moderate risk, 3 = high risk
+#     return_percentages = np.zeros((eras, models, scenarios, snowpacks, 4))
+#     for era in range(eras):
+#         for model in range(models):
+#             for scenario in range(scenarios):
+#                 for snowpack in range(snowpacks):
+#                     # Generates an index out of the current iteration
+#                     # for use with the data_arr that has a flat shape
+#                     # that matches the below code.
+#                     index = (
+#                         (era * models * scenarios * snowpacks)
+#                         + (model * scenarios * snowpacks)
+#                         + (scenario * snowpacks)
+#                         + snowpack
+#                     )
+#                     slice = data_arr[index]
 
-                    # Generates a data array that has all NAN values removed
-                    rm_nan_slice = slice[~np.isnan(slice)]
+#                     # Generates a data array that has all NAN values removed
+#                     rm_nan_slice = slice[~np.isnan(slice)]
 
-                    # If there is no data in this slice, set the
-                    # value and percentages to 0 and continue onto the next data slice.
-                    if len(rm_nan_slice) == 0:
-                        return_arr[era][model][scenario][snowpack] = 0
-                        return_percentages[era][model][scenario][snowpack][1] = 0
-                        return_percentages[era][model][scenario][snowpack][2] = 0
-                        return_percentages[era][model][scenario][snowpack][3] = 0
-                        continue
+#                     # If there is no data in this slice, set the
+#                     # value and percentages to 0 and continue onto the next data slice.
+#                     if len(rm_nan_slice) == 0:
+#                         return_arr[era][model][scenario][snowpack] = 0
+#                         return_percentages[era][model][scenario][snowpack][1] = 0
+#                         return_percentages[era][model][scenario][snowpack][2] = 0
+#                         return_percentages[era][model][scenario][snowpack][3] = 0
+#                         continue
 
-                    # Generates counts for all 1, 2, or 3 values for beetle risk
-                    uniques = np.unique(rm_nan_slice, return_counts=True)
+#                     # Generates counts for all 1, 2, or 3 values for beetle risk
+#                     uniques = np.unique(rm_nan_slice, return_counts=True)
 
-                    # Sets the "mode" to the first of the unique values
-                    # Must have at least one value to have gotten to this part
-                    # of the script.
-                    mode = uniques[0][0]
-                    mode_count = uniques[1][0]
+#                     # Sets the "mode" to the first of the unique values
+#                     # Must have at least one value to have gotten to this part
+#                     # of the script.
+#                     mode = uniques[0][0]
+#                     mode_count = uniques[1][0]
 
-                    # Sets the return percentage of the first count value
-                    # If the mode is 1 above, it will take the count for all 1's
-                    # and divide that by the total size of the array to get the
-                    # percentage of the area that is low risk
-                    return_percentages[era][model][scenario][snowpack][int(mode)] = (
-                        round(mode_count / len(rm_nan_slice) * 100)
-                    )
+#                     # Sets the return percentage of the first count value
+#                     # If the mode is 1 above, it will take the count for all 1's
+#                     # and divide that by the total size of the array to get the
+#                     # percentage of the area that is low risk
+#                     return_percentages[era][model][scenario][snowpack][int(mode)] = (
+#                         round(mode_count / len(rm_nan_slice) * 100)
+#                     )
 
-                    # If the uniques variable has more than one value, we need to
-                    # check to see if this value should actually be the mode of the
-                    # dataset.
-                    if len(uniques[0]) > 1:
-                        for i in range(1, len(uniques[0])):
-                            # Sets the percentage for this risk value
-                            return_percentages[era][model][scenario][snowpack][
-                                int(uniques[0][i])
-                            ] = round(uniques[1][i] / len(rm_nan_slice) * 100)
+#                     # If the uniques variable has more than one value, we need to
+#                     # check to see if this value should actually be the mode of the
+#                     # dataset.
+#                     if len(uniques[0]) > 1:
+#                         for i in range(1, len(uniques[0])):
+#                             # Sets the percentage for this risk value
+#                             return_percentages[era][model][scenario][snowpack][
+#                                 int(uniques[0][i])
+#                             ] = round(uniques[1][i] / len(rm_nan_slice) * 100)
 
-                            # If the count of the uniques for the next value is higher
-                            # than the current mode count, change the mode and count of
-                            # the mode.
-                            if uniques[1][i] > mode_count:
-                                mode = uniques[0][i]
-                                mode_count = uniques[1][i]
-                    return_arr[era][model][scenario][snowpack] = int(mode)
-    return return_arr, return_percentages
+#                             # If the count of the uniques for the next value is higher
+#                             # than the current mode count, change the mode and count of
+#                             # the mode.
+#                             if uniques[1][i] > mode_count:
+#                                 mode = uniques[0][i]
+#                                 mode_count = uniques[1][i]
+#                     return_arr[era][model][scenario][snowpack] = int(mode)
+#     return return_arr, return_percentages
 
 
-def get_poly_mask_arr(ds, poly, bandname):
-    """Get the polygon mask array from an xarray dataset, intended to be recycled for rapid
-    zonal summary across results from multiple WCS requests for the same bbox. Wrapper for
-    rasterstats zonal_stats().
+# def get_poly_mask_arr(ds, poly, bandname):
+#     """Get the polygon mask array from an xarray dataset, intended to be recycled for rapid
+#     zonal summary across results from multiple WCS requests for the same bbox. Wrapper for
+#     rasterstats zonal_stats().
 
-    Args:
-        ds (xarray.DataSet): xarray dataset returned from fetching a bbox from a coverage
-        poly (shapely.Polygon): polygon to create mask from
-        bandname (str): name of the DataArray containing the data
+#     Args:
+#         ds (xarray.DataSet): xarray dataset returned from fetching a bbox from a coverage
+#         poly (shapely.Polygon): polygon to create mask from
+#         bandname (str): name of the DataArray containing the data
 
-    Returns:
-        cropped_poly_mask (numpy.ma.core.MaskedArra): a masked array masking the cells
-            intersecting the polygon of interest, cropped to the right shape
-    """
-    # need a data layer of same x/y shape just for running a zonal stats
-    xy_shape = ds[bandname].values.shape[-2:]
-    data_arr = np.zeros(xy_shape)
-    # get affine transform from the xarray.DataSet
-    ds.rio.set_spatial_dims("X", "Y")
-    transform = ds.rio.transform()
-    poly_mask_arr = zonal_stats(
-        poly,
-        data_arr,
-        affine=transform,
-        nodata=np.nan,
-        stats=["mean"],
-        raster_out=True,
-    )[0]["mini_raster_array"]
-    cropped_poly_mask = poly_mask_arr[0 : xy_shape[1], 0 : xy_shape[0]]
-    return cropped_poly_mask
+#     Returns:
+#         cropped_poly_mask (numpy.ma.core.MaskedArra): a masked array masking the cells
+#             intersecting the polygon of interest, cropped to the right shape
+#     """
+#     # need a data layer of same x/y shape just for running a zonal stats
+#     xy_shape = ds[bandname].values.shape[-2:]
+#     data_arr = np.zeros(xy_shape)
+#     # get affine transform from the xarray.DataSet
+#     ds.rio.set_spatial_dims("X", "Y")
+#     transform = ds.rio.transform()
+#     poly_mask_arr = zonal_stats(
+#         poly,
+#         data_arr,
+#         affine=transform,
+#         nodata=np.nan,
+#         stats=["mean"],
+#         raster_out=True,
+#     )[0]["mini_raster_array"]
+#     cropped_poly_mask = poly_mask_arr[0 : xy_shape[1], 0 : xy_shape[0]]
+#     return cropped_poly_mask
 
 
 def run_aggregate_var_polygon(poly_id):
@@ -335,18 +353,17 @@ def run_aggregate_var_polygon(poly_id):
     Notes:
         Fetches data on the individual instances of the singular dimension combinations. Consider validating polygon IDs in `validate_data` or `lat_lon` module.
     """
-    poly = get_poly(poly_id)
+    polygon = get_poly(poly_id)
+    ds = asyncio.run(fetch_beetles_bbox_data(polygon.total_bounds, beetle_coverage_id))
 
-    ds_list = asyncio.run(fetch_beetles_bbox_data(poly.bounds, beetle_coverage_id))
+    # poly_mask_arr = get_poly_mask_arr(ds_list[0], poly, bandname)
+    # agg_results, risk_percentages = summarize_within_poly_marr(
+    #     ds_list[-1], poly_mask_arr, bandname
+    # )
 
-    bandname = "Gray"
-    poly_mask_arr = get_poly_mask_arr(ds_list[0], poly, bandname)
+    package_beetle_data(agg_results, risk_percentages)
 
-    agg_results, risk_percentages = summarize_within_poly_marr(
-        ds_list[-1], poly_mask_arr, bandname
-    )
-
-    return package_beetle_data(agg_results, risk_percentages)
+    return
 
 
 @routes.route("/beetles/")
