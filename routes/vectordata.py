c6cd1b9f6984611ba1cf9c2012d528255437b090
Bob Torgerson <rltorgerson@alaska.edu>
2025-08-04 11:43:01 -0800

    Adds comments for clarity. Adds dependency of thefuzz to initial install of EB Conda environment.

--- routes/vectordata.py
+++ routes/vectordata.py
@@ -344,7 +344,7 @@ def get_communities():
     GET function to return communities filtered by extent and substring.
     Query params:
         extent: alaska, blockyAlaska, elevation, mizukami, or slie (GeoJSON region)
-        substring: substring to match in name or alt_name
+        substring: substring to match in name or alt_name. Also performs fuzzy matching.
     Returns:
         JSON of filtered communities.
     """
@@ -368,12 +368,16 @@ def get_communities():
             alt_name = community["properties"].get("alt_name", "").lower()
             community_id = community["properties"].get("id")
             if substring in name or substring in alt_name:
-                if community_id not in seen_ids:
-                    filtered_exact.append(community)
-                    seen_ids.add(community_id)
+                filtered_exact.append(community)
+                # Add community ID to the seen_ids set to avoid duplicates
+                seen_ids.add(community_id)
 
+            # Runs fuzzy matching for names and alt_names that are
+            # one character off or very similar to the substring.
             ratio_name = fuzz.ratio(substring, name)
             ratio_alt = fuzz.ratio(substring, alt_name) if alt_name else 0
+            # If the Levenshtein ratio is above 85% and the community ID
+            # has not been added to the set, add it to the filtered list.
             if (ratio_name >= 85 or ratio_alt >= 85) and community_id not in seen_ids:
                 filtered_fuzzy.append(community)
                 seen_ids.add(community_id)
