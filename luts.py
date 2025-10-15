962d166a8255e21dd38c9439f23e7055a8b60282
Craig Stephenson <crstephenson@alaska.edu>
2025-10-15 15:01:47 -0800

    Remove GFDL-ESM4 for now and change 7ModelAvg to 6ModelAvg.

--- luts.py
+++ luts.py
@@ -563,11 +563,10 @@ demographics_order = [
 
 cmip6_downscaled_options = {
     "tasmax": {
-        "7ModelAvg": ["historical", "ssp126", "ssp245", "ssp370", "ssp585"],
+        "6ModelAvg": ["historical", "ssp126", "ssp245", "ssp370", "ssp585"],
         "CNRM-CM6-1-HR": ["historical", "ssp126", "ssp585"],
         "E3SM-2-0": ["historical", "ssp370"],
         "EC-Earth3-Veg": ["historical", "ssp370", "ssp585"],
-        "GFDL-ESM4": ["historical", "ssp126", "ssp245", "ssp370", "ssp585"],
         "HadGEM3-GC31-LL": ["historical", "ssp126", "ssp245", "ssp585"],
         "HadGEM3-GC31-MM": ["historical", "ssp126", "ssp585"],
         "KACE-1-0-G": ["historical", "ssp126", "ssp245", "ssp370", "ssp585"],
@@ -578,11 +577,10 @@ cmip6_downscaled_options = {
         "TaiESM1": ["historical", "ssp126", "ssp245", "ssp370", "ssp585"],
     },
     "tasmin": {
-        "7ModelAvg": ["historical", "ssp126", "ssp245", "ssp370", "ssp585"],
+        "6ModelAvg": ["historical", "ssp126", "ssp245", "ssp370", "ssp585"],
         "CNRM-CM6-1-HR": ["historical", "ssp126", "ssp585"],
         "E3SM-2-0": ["historical", "ssp370"],
         "EC-Earth3-Veg": ["historical", "ssp126", "ssp370", "ssp585"],
-        "GFDL-ESM4": ["historical", "ssp126", "ssp245", "ssp370", "ssp585"],
         "HadGEM3-GC31-LL": ["historical", "ssp126", "ssp245", "ssp585"],
         "HadGEM3-GC31-MM": ["historical", "ssp126", "ssp585"],
         "KACE-1-0-G": ["historical", "ssp126", "ssp245", "ssp585"],
@@ -593,12 +591,11 @@ cmip6_downscaled_options = {
         "TaiESM1": ["historical", "ssp126", "ssp245", "ssp370", "ssp585"],
     },
     "pr": {
-        "7ModelAvg": ["historical", "ssp126", "ssp245", "ssp370", "ssp585"],
+        "6ModelAvg": ["historical", "ssp126", "ssp245", "ssp370", "ssp585"],
         "CESM2": ["historical", "ssp126", "ssp585"],
         "CNRM-CM6-1-HR": ["historical", "ssp126", "ssp585"],
         "E3SM-2-0": ["historical", "ssp370"],
         "EC-Earth3-Veg": ["historical", "ssp126", "ssp370", "ssp585"],
-        "GFDL-ESM4": ["historical", "ssp126", "ssp245", "ssp370", "ssp585"],
         "HadGEM3-GC31-LL": ["historical", "ssp126", "ssp245", "ssp585"],
         "HadGEM3-GC31-MM": ["historical", "ssp126", "ssp585"],
         "KACE-1-0-G": ["historical", "ssp126", "ssp245", "ssp370", "ssp585"],
