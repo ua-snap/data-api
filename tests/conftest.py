0dd3b09c6704431f1167db1f1234a970edf5e06f
Charles Parr <cparr4@alaska.edu>
2025-10-21 10:22:18 -0800

    initial commit area query test rig

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from application import application
+
+@pytest.fixture
+def client():
+    """Create a Flask test client."""
+    with application.test_client() as client:
+        yield client
