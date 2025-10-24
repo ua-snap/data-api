5826f1f13028ebf61936d190d2ebbc2c80c0aacf
Craig Stephenson <crstephenson@alaska.edu>
2025-10-23 19:46:20 -0800

    Add more info about endpoints and endpoint options to documentation page.

--- luts.py
+++ luts.py
@@ -580,7 +580,7 @@ cmip6_downscaled_options = {
         "6ModelAvg": ["historical", "ssp126", "ssp245", "ssp370", "ssp585"],
         "CNRM-CM6-1-HR": ["historical", "ssp126", "ssp585"],
         "E3SM-2-0": ["historical", "ssp370"],
-        "EC-Earth3-Veg": ["historical", "ssp126", "ssp370", "ssp585"],
+        "EC-Earth3-Veg": ["historical", "ssp370", "ssp585"],
         "HadGEM3-GC31-LL": ["historical", "ssp126", "ssp245", "ssp585"],
         "HadGEM3-GC31-MM": ["historical", "ssp126", "ssp585"],
         "KACE-1-0-G": ["historical", "ssp126", "ssp245", "ssp370", "ssp585"],
