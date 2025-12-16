7cfb47f2fc236482a209ca17c976e292e383a954
joshdpaul <jdpaul3@alaska.edu>
2025-12-16 10:14:23 -0900

    consolidate stats functions, packaging works

--- routes/conus_hydrology.py
+++ routes/conus_hydrology.py
@@ -26,10 +26,14 @@ from . import routes
 # TODO: Add validation of stream ID
 # TODO: Improve error handling throughout
 
-seg_cov_id = "conus_hydro_segments_all_stats"
-
+seg_cov_id = "conus_hydro_segments"
 hydrograph_hist_cov_id = "conus_hydro_segments_doy_mmm_maurer_historical_test_2"
 hydrograph_proj_cov_id = "conus_hydro_segments_doy_mmm_maurer_projected_test"
+
+
+async def get_decode_dicts_from_axis_attributes(cov_id):
+    metadata = await describe_via_wcps(cov_id)
+    return get_axis_encodings(metadata)
 
 
 def fetch_hydro_data(cov_id, stream_id, type):
@@ -49,127 +53,51 @@ def fetch_hydro_data(cov_id, stream_id, type):
     return ds
 
 
-async def get_decode_dicts_from_axis_attributes(cov_id):
-    metadata = await describe_via_wcps(cov_id)
-    return get_axis_encodings(metadata)
-
-
-async def get_decode_dicts_from_coverage_attributes(cov_id):
-    metadata = await describe_via_wcps(cov_id)
-    return get_coverage_encodings(metadata)
-
-
-# TODO: replace this function with get_decode_dicts_from_axis_attributes()
-def build_decode_dicts(seg_encoding_attr):
-    """
-    Function to build decoding dictionaries.
-    Searches the XML response from the DescribeCoverage request for the encodings metadata and
-    returns the dictionary of encodings. Reverses the dictionary of encodingsd so we can decodes
-    and return dimensions as strings.
-    Args:
-        ds (xarray dataset): Dataset with hydrological stats for the stream ID
-        seg_encoding_attr (str): Attribute name for the encoding dictionary in the XML response
-    Returns:
-        Decoded data dictionary with human-readable keys."""
-
-    url = (
-        RAS_BASE_URL
-        + f"ows?&SERVICE=WCS&VERSION=2.1.0&REQUEST=DescribeCoverage&COVERAGEID={seg_cov_id}&outputType=GeneralGridCoverage"
-    )
-    with requests.get(url, verify=False) as r:
-        if r.status_code != 200:
-            return render_template("500/server_error.html"), 500
-        tree = ET.ElementTree(ET.fromstring(r.content))
-
-    xml_search_string = str(".//{http://www.rasdaman.org}" + seg_encoding_attr)
-    encoding_dict_str = tree.findall(xml_search_string)[0].text
-    encoding_dict = eval(encoding_dict_str)
-
-    # for each dimension, reverse the encoding dictionary to decode the keys
-    # also convert to float, since this is the dtype of the encoded values in the datacube
-    # TODO: fix netCDF encoding to use integers instead of floats!
-    lc_dict = {float(v): k for k, v in encoding_dict["lc"].items()}
-    model_dict = {float(v): k for k, v in encoding_dict["model"].items()}
-    scenario_dict = {float(v): k for k, v in encoding_dict["scenario"].items()}
-    era_dict = {float(v): k for k, v in encoding_dict["era"].items()}
-
-    return lc_dict, model_dict, scenario_dict, era_dict
-
-
-# TODO: condense into a package_stats_data() function
-def build_dict_and_populate_stats(stream_id, ds):
-    """
-    Function to populate the stats in the data dictionary with the hydrology statistics.
-    The levels of the stats data dictionary are as follows: landcover, model, scenario, era, variable.
-    Args:
-        stream_id (str): Stream ID for the hydrology data
-        ds (xarray dataset): Dataset with hydrological stats for the stream ID
-        data_dict (dict): Data dictionary to populate with the hydrology stats
-    Returns:
-        Data dictionary with the hydrology stats populated.
-    """
-
-    lc_dict, model_dict, scenario_dict, era_dict = build_decode_dicts(seg_encoding_attr)
-
-    data_dict = {
-        stream_id: {"name": None, "latitude": None, "longitude": None, "stats": {}}
+def package_stats_data(stream_id, ds):
+    stats_dict = {
+        stream_id: {"name": None, "latitude": None, "longitude": None, "data": {}}
     }
 
-    # get the stats from the dataset for each landcover, model, scenario, era, and variable.
-    vars = list(ds.data_vars)
-    for lc in ds.lc.values:
-        data_dict[stream_id]["stats"][lc_dict[lc]] = {}
+    for landcover in ds.landcover.values:
+        if landcover not in stats_dict[stream_id]["data"]:
+            stats_dict[stream_id]["data"][landcover] = {}
         for model in ds.model.values:
-            data_dict[stream_id]["stats"][lc_dict[lc]][model_dict[model]] = {}
+            if model not in stats_dict[stream_id]["data"][landcover]:
+                stats_dict[stream_id]["data"][landcover][model] = {}
             for scenario in ds.scenario.values:
-                data_dict[stream_id]["stats"][lc_dict[lc]][model_dict[model]][
-                    scenario_dict[scenario]
-                ] = {}
-                # if scenario is historical, get only the first era values (all others are null)
-                if scenario_dict[scenario] == "historical":
-                    for era in ds.era.values[:1]:
-                        data_dict[stream_id]["stats"][lc_dict[lc]][model_dict[model]][
-                            scenario_dict[scenario]
-                        ][era_dict[era]] = {}
-                        stats_dict = {}
-                        for var in vars:
-                            stat_value = float(
-                                ds[var].sel(
-                                    lc=lc, model=model, scenario=scenario, era=era
-                                )
+                if scenario not in stats_dict[stream_id]["data"][landcover][model]:
+                    stats_dict[stream_id]["data"][landcover][model][scenario] = {}
+                for era in ds.era.values:
+
+                    var_dict = {}
+                    for var in list(ds.data_vars):
+                        stat_value = (
+                            ds[var]
+                            .sel(
+                                landcover=landcover,
+                                model=model,
+                                scenario=scenario,
+                                era=era,
                             )
+                            .values
+                        ).item()
 
-                            if np.isnan(stat_value):
-                                stat_value = None
+                        if np.isnan(stat_value):
+                            stat_value = None
+                        else:
+                            stat_value = round(stat_value, 2)
 
-                            stats_dict[var] = stat_value
+                        var_dict[var] = {
+                            "value": stat_value,
+                            "units": ds[var].attrs.get("units", ""),
+                            # "description": ds[var].attrs.get("description", ""),
+                        }
 
-                            data_dict[stream_id]["stats"][lc_dict[lc]][
-                                model_dict[model]
-                            ][scenario_dict[scenario]][era_dict[era]] = stats_dict
-                # if scenario is not historical, get all era values except the first (which is null)
-                else:
-                    for era in ds.era.values[1:]:
-                        data_dict[stream_id]["stats"][lc_dict[lc]][model_dict[model]][
-                            scenario_dict[scenario]
-                        ][era_dict[era]] = {}
-                        stats_dict = {}
-                        for var in vars:
-                            stat_value = float(
-                                ds[var].sel(
-                                    lc=lc, model=model, scenario=scenario, era=era
-                                )
-                            )
+                    stats_dict[stream_id]["data"][landcover][model][scenario][
+                        era
+                    ] = var_dict
 
-                            if np.isnan(stat_value):
-                                stat_value = None
-
-                            stats_dict[var] = stat_value
-                            data_dict[stream_id]["stats"][lc_dict[lc]][
-                                model_dict[model]
-                            ][scenario_dict[scenario]][era_dict[era]] = stats_dict
-
-    return data_dict
+    return stats_dict
 
 
 # TODO: condense into a package_stats_data() function
@@ -299,21 +227,20 @@ def run_get_conus_hydrology_stats_data(stream_id):
     Returns:
         JSON response with hydrological stats for the requested stream ID.
     """
-
-    # testing
-    # print("ORIGINAL ENCODINGS OUTPUT:")
-    # print(build_decode_dicts(seg_encoding_attr), "\n\n")
+    # fetch data and metadata
     decode_dict = asyncio.run(get_decode_dicts_from_axis_attributes(seg_cov_id))
-    print("NEW ENCODINGS OUTPUT:")
-    print(decode_dict)
-
     ds = fetch_hydro_data(seg_cov_id, stream_id, type="stats")
+    # decode the dimension values
+    for dim in decode_dict.keys():
+        decoded_vals = [decode_dict[dim][float(v)] for v in ds[dim].values]
+        ds[dim] = decoded_vals
+    # package the stats data into a dictionary for JSON serialization
+    data_dict = package_stats_data(stream_id, ds)
 
-    # build the data dictionary and populate with the hydrology statistics
-    data_dict = build_dict_and_populate_stats(stream_id, ds)
+    # TODO: populate attributes from vector data
+    # data_dict = get_features_and_populate_attributes(data_dict)
 
-    # populate attributes from vector data
-    data_dict = get_features_and_populate_attributes(data_dict)
+    # TODO: prune nulls
 
     # convert to JSON
     json_results = json.dumps(data_dict, indent=4)
@@ -331,6 +258,7 @@ def run_get_conus_hydrology_hydrograph(stream_id):
     Returns:
         JSON response with hydrograph data for the requested stream ID.
     """
+    # fetch data and metadata
     decode_dict_hist = asyncio.run(
         get_decode_dicts_from_axis_attributes(hydrograph_hist_cov_id)
     )
@@ -344,28 +272,22 @@ def run_get_conus_hydrology_hydrograph(stream_id):
         hydrograph_proj_cov_id, stream_id, type="hydrograph_proj"
     )
 
-    # Decode dimensions in the dataset to strings using the decode dictionaries
+    # decode the dimension values
     for dim in decode_dict_hist.keys():
         decoded_vals = [decode_dict_hist[dim][float(v)] for v in ds_hist[dim].values]
         ds_hist[dim] = decoded_vals
-
     for dim in decode_dict_proj.keys():
         decoded_vals = [decode_dict_proj[dim][float(v)] for v in ds_proj[dim].values]
         ds_proj[dim] = decoded_vals
 
-    # Print for debugging
-    print(ds_hist)
-    print(ds_proj)
-
     # Package the hydrograph data into a dictionary for JSON serialization
     data_dict = package_hydrograph_data(stream_id, ds_hist, ds_proj)
 
-    # TODO: this pruning only works for lowest dict level - how to remove empty scenario/era combinations?
-    # prune twice does not work to remove all the missing scenario / era combinations
-    pruned_data_dict = prune_nulls_with_max_intensity(data_dict)
-    pruned_data_dict = prune_nulls_with_max_intensity(pruned_data_dict)
+    # TODO: populate attributes from vector data
+    # data_dict = get_features_and_populate_attributes(data_dict)
+    # TODO: prune nulls
 
     # Convert to JSON
-    json_results = json.dumps(pruned_data_dict, indent=4)
+    json_results = json.dumps(data_dict, indent=4)
 
     return Response(json_results, mimetype="application/json")
