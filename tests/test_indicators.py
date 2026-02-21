926d94903c978f488acf4a0a8a75d2330db1fd60
Charles Parr <cparr4@alaska.edu>
2026-02-20 15:37:42 -0900

    add trailing slash to route

--- tests/test_indicators.py
+++ tests/test_indicators.py
@@ -3,10 +3,10 @@ import json
 
 def test_indicators_cmip5_area(client):
     """
-    Tests the /indicators/cmip5/area/<id> endpoint to ensure the output
+    Tests the /indicators/cmip5/area/<id>/ endpoint to ensure the output
     remains consistent with production for the given area ID.
     """
-    response = client.get("/indicators/cmip5/area/1903040601")
+    response = client.get("/indicators/cmip5/area/1903040601/")
     assert response.status_code == 200
     actual_data = response.get_json()
 
